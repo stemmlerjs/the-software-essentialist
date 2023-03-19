ebfda42b63ba060e54c1b6c472a8fd1505da23b4
Rene Corrales <ruckonic@icloud.com>
2023-03-19 16:23:37 -0600

    RED: should throw an error

---
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -0,0 +1,11 @@
+/**
+ * FizzBuzz returns a string "Fizz" when a number is multiple of three and for multiples of five it returns “Buzz.” 
+ * For numbers that are multiples of both three and five, it returns “FizzBuzz.”
+ * @param input  number from 1 to 100
+ * @throws if any number is out of range 1 to 100
+ */
+export function fizzbuzz(input: number) {
+  // ...
+}
+
+export default fizzbuzz
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,9 @@
+import fizzbuzz from './fizzbuzz'
 
 describe("fizzbuzz", () => {
 
+  test.each([101, -1, 0])("should throw an error when number is outside the range 1 to 100", (n) => {
+    expect(fizzbuzz(n)).toThrow(RangeError)
+  })
 
 });
