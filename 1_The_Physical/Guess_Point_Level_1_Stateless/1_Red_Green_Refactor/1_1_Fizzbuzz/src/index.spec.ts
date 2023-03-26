8290330ad0c1c637951b09bef9c8910a659269a2
Anton Krychevskyy <Anton.Krychevskyy@gmail.com>
2023-03-26 17:59:36 +0300

    [prepare]: fizzbuzz - adds comments list in test file with functional requirements

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,10 @@
+// import { fizzbuzz} from './fizzbuzz';
 
 describe("fizzbuzz", () => {
-
-
+  // TODO: accepts numbers as arguments and returns thir string representation
+  // TODO: requires at least one argument, returns "Invalid input" on zero arguments
+  // TODO: requires numbers to between 1 and 100 only, throws "Invalid input" on out-of-range numbers
+  // TODO: returns "Fizz" for multiples of 3
+  // TODO: returns "Buzz" for multiples of 5
+  // TODO: returns "FizzBuzz" for multiples of 3 and 5
 });
