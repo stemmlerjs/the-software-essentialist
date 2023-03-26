369a8480bf81618a7cc7ff41b066a8a6279ad2db
Anton Krychevskyy <Anton.Krychevskyy@gmail.com>
2023-03-26 20:48:21 +0300

    [new]: fizzbuzz - accepts number and returns its string represetation

---
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -0,0 +1,3 @@
+export const fizzbuzz = (number: number): string => {
+  return number.toString();
+};
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,7 +1,15 @@
-// import { fizzbuzz} from './fizzbuzz';
+/// <reference types="jest" />
+import { fizzbuzz } from './fizzbuzz';
 
-describe("fizzbuzz", () => {
-  // TODO: accepts numbers as arguments and returns thir string representation
+describe('fizzbuzz', () => {
+  // TODO: accepts numbers as arguments and returns its string representation
+  it('accepts number as argument and returns its string representation', () => {
+    expect(typeof fizzbuzz(1)).toBe('string');
+    expect(typeof fizzbuzz(27)).toBe('string');
+
+    expect(fizzbuzz(8)).toBe('8');
+    expect(fizzbuzz(41)).toBe('41');
+  });
   // TODO: requires at least one argument, returns "Invalid input" on zero arguments
   // TODO: requires numbers to between 1 and 100 only, throws "Invalid input" on out-of-range numbers
   // TODO: returns "Fizz" for multiples of 3
