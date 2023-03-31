f9a00d848f845e9cfcab3c019fe8c3ec8a289e17
Anton Krychevskyy <Anton.Krychevskyy@gmail.com>
2023-03-31 19:05:55 +0300

    [new] - palindrome - throws error if there is no argument, if it is not a string or string contains less then 3 letters

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -7,6 +7,11 @@ describe('palindrome checker', () => {
     expect(palindromeChecker('mom')).toBe(true);
   });
   // TODO: throws an error if there is no input or input is not a string or string contains less than 3 characters
+  it('should throw an error if input is invalid', () => {
+    expect(() => palindromeChecker()).toThrowError('Input should be provided');
+    expect(() => palindromeChecker(3)).toThrowError('Input should be a string');
+    expect(() => palindromeChecker('   ')).toThrowError('Input should contain at least 3 letters');
+  });
   // TODO: returns true if the input is a palindrome and false if not
   // TODO: returns true if the input is a palindrome with different letter cases
   // TODO: returns true if the input is a palindrome with spaces
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,3 +1,8 @@
 export const palindromeChecker = (input: string): boolean => {
+  if (typeof input === 'undefined') throw new Error('Input should be provided');
+  if (typeof input !== 'string') throw new Error('Input should be a string');
+  if (input.replaceAll(/[^\w]|_/g, '').length < 3)
+    throw new Error('Input should contain at least 3 letters');
+
   return true;
 };
