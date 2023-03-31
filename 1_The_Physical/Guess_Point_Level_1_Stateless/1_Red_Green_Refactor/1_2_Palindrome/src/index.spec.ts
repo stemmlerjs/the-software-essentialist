5acde7e366f2d13b92eebb50b929c5af0f415e48
Anton Krychevskyy <Anton.Krychevskyy@gmail.com>
2023-03-31 18:34:12 +0300

    [prepare] - palindrome - adds testing plan comments list

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,4 +1,7 @@
-
 describe('palindrome checker', () => {
-
-})
+  // TODO: accepts a string and returns a boolean
+  // TODO: throws an error if there is no input or input is not a string or string contains less than 3 characters
+  // TODO: returns true if the input is a palindrome and false if not
+  // TODO: returns true if the input is a palindrome with different letter cases
+  // TODO: returns true if the input is a palindrome with spaces
+});
