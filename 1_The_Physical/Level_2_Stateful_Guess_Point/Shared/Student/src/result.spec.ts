1c9f79b30180c1cf594b8ea785394b585fdcfd73
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-05-09 22:20:31 +0200

    🟢 create successful result with a custom object

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/result.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/result.spec.ts
@@ -1,16 +1,31 @@
 import { Result } from "./result";
 
 describe("Result", () => {
-  describe("when creating a successful Result with 'This is a value' as value", () => {
-    it("returns a Result object with a value", () => {
-      // Arrange
-      const value = "This is a value";
+  describe("when creating a successful Result", () => {
+    describe("with 'This is a value' as value", () => {
+      it("returns a Result object with a value", () => {
+        // Arrange
+        const value = "This is a value";
 
-      // Act
-      const result = Result.success(value);
+        // Act
+        const result = Result.success(value);
 
-      // Assert
-      expect(result).toEqual(expect.objectContaining({ value }));
+        // Assert
+        expect(result).toEqual(expect.objectContaining({ value }));
+      });
+    });
+
+    describe("with a custom object as value", () => {
+      it("returns a Result object with a value", () => {
+        // Arrange
+        const value = { foo: "bar" };
+
+        // Act
+        const result = Result.success(value);
+
+        // Assert
+        expect(result).toEqual(expect.objectContaining({ value }));
+      });
     });
   });
 });
