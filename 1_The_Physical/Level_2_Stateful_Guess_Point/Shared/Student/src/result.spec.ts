70d73424c6e84f0689a9a1bd1d3176a5197fe2a6
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-05-09 22:26:02 +0200

    🔵 refactor 'Result' tests to improve readability and reduce repetition

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/result.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/result.spec.ts
@@ -1,31 +1,16 @@
 import { Result } from "./result";
 
 describe("Result", () => {
-  describe("when creating a successful Result", () => {
-    describe("with 'This is a value' as value", () => {
-      it("returns a Result object with a value", () => {
-        // Arrange
-        const value = "This is a value";
-
+  describe.each([["This is a value"], [{ foo: "bar" }]])(
+    "when creating a successful Result with value %s",
+    (value) => {
+      it("returns a Result object with the value", () => {
         // Act
         const result = Result.success(value);
 
         // Assert
         expect(result).toEqual(expect.objectContaining({ value }));
       });
-    });
-
-    describe("with a custom object as value", () => {
-      it("returns a Result object with a value", () => {
-        // Arrange
-        const value = { foo: "bar" };
-
-        // Act
-        const result = Result.success(value);
-
-        // Assert
-        expect(result).toEqual(expect.objectContaining({ value }));
-      });
-    });
-  });
+    }
+  );
 });
