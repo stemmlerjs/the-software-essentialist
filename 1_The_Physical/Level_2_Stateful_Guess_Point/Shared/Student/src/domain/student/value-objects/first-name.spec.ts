8669577626b88a826c271670ae5cea4dfb8686ea
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-05-10 15:52:57 +0200

    🟢 add validation and rearrange tests for 'FirstName' value-object

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/domain/student/value-objects/first-name.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/domain/student/value-objects/first-name.spec.ts
@@ -3,9 +3,82 @@ import { FirstName } from "./first-name";
 describe("FirstName", () => {
   describe("when creating a first name", () => {
     it("returns a first name instance with value 'John'", () => {
-      const firstName = FirstName.create("John");
+      // Arrange
+      const firstName = "John";
 
-      expect(firstName.value).toBe("John");
+      // Act
+      const result = FirstName.create(firstName);
+
+      // Assert
+      expect(result.value?.value).toBe(firstName);
+    });
+  });
+
+  describe("when student's first name is not provided", () => {
+    it("returns a FirstNameValidationError object with a 'required' message", () => {
+      // Arrange
+      const firstName = "";
+
+      // Act
+      const result = FirstName.create(firstName);
+
+      // Assert
+      expect(result.error).toEqual(
+        expect.objectContaining({
+          required: "Firstname is required",
+        })
+      );
+    });
+  });
+
+  describe("when student's first name is less than 2 characters long", () => {
+    it("returns a FirstNameValidationError object with a 'min' message", () => {
+      // Arrange
+      const firstName = "J";
+
+      // Act
+      const result = FirstName.create(firstName);
+
+      // Assert
+      expect(result.error).toEqual(
+        expect.objectContaining({
+          min: "Firstname must be at least 2 characters long",
+        })
+      );
+    });
+  });
+
+  describe("when student's first name is more than 10 characters long", () => {
+    it("returns a FirstNameValidationError object with a 'max' message", () => {
+      // Arrange
+      const firstName = "JohnJohnJohn";
+
+      // Act
+      const result = FirstName.create(firstName);
+
+      // Assert
+      expect(result.error).toEqual(
+        expect.objectContaining({
+          max: "Firstname must be at most 10 characters long",
+        })
+      );
+    });
+  });
+
+  describe("when student's first name contains non-alphabetic characters", () => {
+    it("returns a FirstNameValidationError object with a 'letters' message", () => {
+      // Arrange
+      const firstName = "John1";
+
+      // Act
+      const result = FirstName.create(firstName);
+
+      // Assert
+      expect(result.error).toEqual(
+        expect.objectContaining({
+          letters: "Firstname must contain only letters",
+        })
+      );
     });
   });
 });
--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/domain/student/value-objects/first-name.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/domain/student/value-objects/first-name.ts
@@ -1,19 +1,64 @@
+import { Result } from "../../../shared/result";
 import { ValueObject } from "../../core/value-object";
 
 interface FirstNameProps {
   value: string;
 }
 
+export interface FirstNameValidationError {
+  min?: string;
+  max?: string;
+  letters?: string;
+  required?: string;
+}
+
 export class FirstName extends ValueObject<FirstNameProps> {
+  static readonly minLength = 2;
+  static readonly maxLength = 10;
+
   private constructor(props: FirstNameProps) {
     super(props);
   }
 
-  get value(): string {
-    return this.props.value;
+  public static create(
+    value: string
+  ): Result<FirstName, FirstNameValidationError> {
+    const errors = FirstName.validate(value);
+
+    if (
+      Object.values(errors).some((props) => Object.values(props).some(Boolean))
+    ) {
+      return Result.failure(errors);
+    }
+
+    return Result.success(new FirstName({ value }));
   }
 
-  public static create(value: string): FirstName {
-    return new FirstName({ value });
+  public static validate(value: string): FirstNameValidationError {
+    const errors: FirstNameValidationError = {};
+
+    if (!value) {
+      errors.required = "Firstname is required";
+    } else {
+      value = value.trim();
+
+      if (value.length < FirstName.minLength) {
+        errors.min = `Firstname must be at least ${FirstName.minLength} characters long`;
+      }
+
+      if (value.length > FirstName.maxLength) {
+        errors.max = `Firstname must be at most ${FirstName.maxLength} characters long`;
+      }
+
+      if (!/^[a-zA-Z]+$/.test(value)) {
+        errors.letters = `Firstname must contain only letters`;
+      }
+    }
+
+    return errors;
+  }
+
+  public get value(): string {
+    return this.props.value;
   }
 }
