35dcaaacea576511b312252f55decce75c190f4e
Khalil Stemmler <metroidman12@gmail.com>
2023-05-11 23:13:59 -0400

    [Green]: returns 'Fizz' for 9

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,5 +1,6 @@
 
 export function fizzBuzz (num: number) {
-  if (num === 15) return 'FizzBuzz'
-  return num === 3 ? 'Fizz' : 'Buzz'
+  if (num === 9) return 'Fizz';
+  if (num === 15) return 'FizzBuzz';
+  return num === 3 ? 'Fizz' : 'Buzz';
 }
