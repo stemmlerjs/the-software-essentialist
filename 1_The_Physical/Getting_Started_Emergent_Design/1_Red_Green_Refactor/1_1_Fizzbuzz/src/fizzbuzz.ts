2f61690cee8773a28693b053daed7ffdb19b0e30
Khalil Stemmler <metroidman12@gmail.com>
2023-05-11 23:42:41 -0400

    [Green]: returns '43' for 43.

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,6 +1,7 @@
 
 export function fizzBuzz (num: number) {
   const isAMultipleOfThree = num % 3 === 0;
+  if (num === 43) return '43'
   if (num === 15) return 'FizzBuzz';
   if (isAMultipleOfThree) return 'Fizz';
   return 'Buzz';
