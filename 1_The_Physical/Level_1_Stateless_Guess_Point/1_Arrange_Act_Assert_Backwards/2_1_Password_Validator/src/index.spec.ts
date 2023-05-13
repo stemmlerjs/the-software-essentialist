1de24cf6859a0552614dab4c6608711e8a9d5a7f
Khalil Stemmler <metroidman12@gmail.com>
2023-05-13 18:54:33 -0400

    Refactor: Refactored the first feature; expressed it using parameterization instead.

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -2,25 +2,42 @@
 import { PasswordValidator } from './index'
 
 describe('password validator', () => {
-  it('knows that "jamiE8" is between 5 and 15 characters long', () => {
-    let output = PasswordValidator.validate('jamiE8');
-    expect(output.result).toBeTruthy();
-    expect(output.errors).toHaveLength(0);
+  describe('checking between 5 and 15 characters long', () => {
+    it.each([
+      ['jamiE8', true, []],
+      ['jaE8', false, ['InvalidLength']],
+      ['thePhysical1234567', false, ['InvalidLength']]
+    ])('knows that "%s" should return %s', 
+      (input: string, result: boolean, errors: string[]) => {
+        let output = PasswordValidator.validate(input);
+        expect(output.result).toBe(result)
+        expect(output.errors).toHaveLength(errors.length);
+        expect(output.errors).toStrictEqual(errors);
+    });
+
+    
   })
+  
 
-  it ('knows that "jaE8" is NOT between 5 and 15 characters long', () => {
-    let output = PasswordValidator.validate('jaE8');
-    expect(output.result).toBeFalsy();
-    expect(output.errors).toHaveLength(1);
-    expect(output.errors[0]).toEqual('InvalidLength')
-  });
+  // it(,  => {
+  //   let output = PasswordValidator.validate('jamiE8');
+  //   expect(output.result).toBeTruthy();
+  //   expect(output.errors).toHaveLength(0);
+  // })
 
-  it ('knows that "thePhysical1234567" is NOT between 5 and 15 characters long', () => {
-    let output = PasswordValidator.validate('thePhysical1234567');
-    expect(output.result).toBeFalsy();
-    expect(output.errors).toHaveLength(1);
-    expect(output.errors[0]).toEqual('InvalidLength')
-  });
+  // it ('knows that "jaE8" is NOT between 5 and 15 characters long', () => {
+  //   let output = PasswordValidator.validate('jaE8');
+  //   expect(output.result).toBeFalsy();
+  //   expect(output.errors).toHaveLength(1);
+  //   expect(output.errors[0]).toEqual('InvalidLength')
+  // });
+
+  // it ('knows that "thePhysical1234567" is NOT between 5 and 15 characters long', () => {
+  //   let output = PasswordValidator.validate('thePhysical1234567');
+  //   expect(output.result).toBeFalsy();
+  //   expect(output.errors).toHaveLength(1);
+  //   expect(output.errors[0]).toEqual('InvalidLength')
+  // });
   
 })
 
