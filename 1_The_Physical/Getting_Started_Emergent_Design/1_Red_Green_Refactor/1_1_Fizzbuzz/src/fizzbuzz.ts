681151c096daf98e127ad23b044174b9bc8c404b
Nader Nasr <nadergnasr@gmail.com>
2023-06-08 23:21:29 +0000

    [Green] fix returns 45 as FizzBuzz

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,7 +1,7 @@
 export function fizzbuzz(value: number): string {
     
-    if (value === 15) return "FizzBuzz";
-    
+    if (value%15 === 0) return "FizzBuzz";
+
     if (value%3 === 0) return "Fizz";
 
     if (value%5 === 0) return "Buzz";
