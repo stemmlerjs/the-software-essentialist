fadb5394ef4495e64884024f9a71941e71dd8c63
Nader Nasr <nadergnasr@gmail.com>
2023-06-08 23:44:19 +0000

    [Green] fix throws an error when no number is passed in

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,4 +1,6 @@
 export function fizzbuzz(value: number): string {
+    if (value === undefined) throw new Error("Input must be provided");
+
     if (value < 1) throw new Error("Input must be greater than 0");
 
     if (value > 100) throw new Error("Input must be no more than 100");
