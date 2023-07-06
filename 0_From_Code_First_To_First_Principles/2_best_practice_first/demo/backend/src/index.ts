d8fcc343c7a33ec4900d722084e56f1ba0548288
Khalil Stemmler <metroidman12@gmail.com>
2023-07-05 23:19:15 -0400

    Encapsulated the concept of a web server.

--- 0_From_Code_First_To_First_Principles/2_best_practice_first/demo/backend/src/index.ts
+++ 0_From_Code_First_To_First_Principles/2_best_practice_first/demo/backend/src/index.ts
@@ -2,32 +2,25 @@ import express, { Request, Response } from 'express';
 import { createUser, editUser, getUserByEmail } from './controllers/userController';
 const cors = require('cors');
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-// Create a new user
-app.post('/users/new', async (req: Request, res: Response) => {
+const createNewUserController = async (req: Request, res: Response) => {
   try {
     const user = await createUser(req.body);
     res.status(201).json(user);
   } catch (error) {
     res.status(500).json({ error: 'Failed to create user.' });
   }
-});
+}
 
-// Edit a user
-app.post('/users/edit/:userId', async (req: Request, res: Response) => {
+const editUserByIdController = async (req: Request, res: Response) => {
   try {
     const user = await editUser(Number(req.params.userId), req.body);
     res.json(user);
   } catch (error) {
     res.status(500).json({ error: 'Failed to edit user.' });
   }
-});
+}
 
-// Get a user by email
-app.get('/users', async (req: Request, res: Response) => {
+const getUserByEmailController = async (req: Request, res: Response) => {
   try {
     const email = req.query.email as string;
     const user = await getUserByEmail(email);
@@ -35,9 +28,54 @@ app.get('/users', async (req: Request, res: Response) => {
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch user.' });
   }
-});
+}
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+type WebServerConfig = {
+  port: number;
+}
+
+export class WebServer {
+  private instance: express.Express;
+
+  constructor (private config: WebServerConfig) {
+    this.instance = this.createInstance();
+    this.configureInstance();
+    this.setupRoutes();
+  }
+
+  private createInstance () {
+    return express();
+  }
+
+  private configureInstance () {
+    this.instance.use(cors());
+    this.instance.use(express.json());
+  }
+
+  private setupRoutes () {
+    // Get a user by email
+    this.instance.get('/users', (req, res) => getUserByEmailController(req, res));
+
+    // Edit a user
+    this.instance.post('/users/edit/:userId', (req, res) => editUserByIdController(req, res));
+
+    // Create a new user
+    this.instance.post('/users/new', (req, res) => createNewUserController(req, res));
+  }
+
+  getExpressApp () {
+    return;
+  }
+
+  async start (): Promise<void> {
+    this.instance.listen(this.config.port, () => {
+      console.log(`Server is running on port ${this.config.port}`);
+    });
+  }
+
+  async stop (): Promise<void> {
+
+  }
+}
+
+new WebServer({ port: 3000 }).start();
