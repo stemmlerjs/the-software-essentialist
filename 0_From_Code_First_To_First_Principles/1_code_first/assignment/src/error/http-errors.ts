b3bd42e3d82f6a54eced496692488cf7e0a84241
Gokhan <gokhanhkocyigit@gmail.com>
2023-07-15 23:33:44 +0200

    feat: add http errors

---
+++ 0_From_Code_First_To_First_Principles/1_code_first/assignment/src/error/http-errors.ts
@@ -0,0 +1,30 @@
+import { HttpCodes } from "../enum/http-codes";
+
+export class ValidationError
+{
+    message: string;
+    code: HttpCodes;
+    constructor() {
+        this.message = "ValidationError";
+        this.code = HttpCodes.BAD_REQUEST;
+    }
+}
+
+export class ConflictError{
+    message: string;
+    code: HttpCodes;
+    constructor(message: string) {
+        this.message = message;
+        this.code = HttpCodes.CONFILCT;
+    }
+}
+
+export class NotFoundError
+{
+    message: string;
+    code: HttpCodes;
+    constructor(message: string) {
+        this.message = message;
+        this.code = HttpCodes.NOT_FOUND;
+    }
+}
