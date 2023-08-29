a9cc75c532ba5702ff3fe831310766f901809e31
Yael <raifer.dev@gmail.com>
2023-08-29 18:15:43 +0300

    [red]: test fizzbuzz returns Fizz

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -5,5 +5,9 @@ describe("fizzbuzz", () => {
   it("should return return a string", () => {
     expect(typeof fizzBuzz(5)).toBe("string");
   });
+
+  it("should return Fizz when input is 3", () => {
+    expect(fizzBuzz(3)).toBe("Fizz");
+  });
  
 });
