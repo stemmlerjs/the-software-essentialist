c847d653a8c62ed168cfdc4fa3b37fbc9fa95c53
Yael <raifer.dev@gmail.com>
2023-08-29 19:10:11 +0300

    [improvement]: changed test names to be more descriptive

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -6,21 +6,22 @@ describe("fizzbuzz", () => {
     expect(typeof fizzBuzz(5)).toBe("string");
   });
 
-  it ("returns multiples of three as 'Fizz'", () => {
+  it ("returns multiples of three such as 3, 6, 9, and 12 as 'Fizz'", () => {
     [3, 6, 9, 12].map((multiple) => fizzBuzz(multiple))
       .forEach((m) => expect(m).toEqual("Fizz"))
   });
- 
-  test("should return 'Buzz' when input is 5", () => {
-    expect(fizzBuzz(5)).toBe("Buzz");
+
+  it("returns multiples of five such as 5, 10, and 20 as buzz", () => {
+    [5, 10, 20].map((multiple) => fizzBuzz(multiple))
+      .forEach((m) => expect(m).toEqual("buzz"));
   });
 
-  it ("returns multiples of three and five as 'FizzBuzz'", () => {
+  it ("returns multiples of both such as 15 and 45 as 'FizzBuzz'", () => {
     [15, 45].map((multiple) => fizzBuzz(multiple))
       .forEach((m) => expect(m).toEqual("FizzBuzz"))
   });
 
-  test("should return '43' when input is 43", () => {
+  test("returns string output of a number such as 43 -> '43'", () => {
     expect(fizzBuzz(43)).toBe("43");
   });
 
