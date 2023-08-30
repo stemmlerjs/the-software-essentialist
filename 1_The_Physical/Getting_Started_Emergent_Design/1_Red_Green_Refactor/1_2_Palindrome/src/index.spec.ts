0d55b0496e518a913f477ac9f8a613feedb35f32
Yael <raifer.dev@gmail.com>
2023-08-30 13:14:46 +0300

    [green]: returns true for  and

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -2,8 +2,26 @@ import { palindromeChecker } from './index';
 
 describe('palindrome checker', () => {
 
+  // "mom" returns true
+  // "Mom" returns true
+  // "MoM" returns true
+  // "Momx" returns false
+  // "xMomx" returns true
+  // "Was It A Rat I Saw" returns true
+  // "Never Odd or Even" returns true
+  // "Never Odd or Even1" returns false 
+  // "1Never Odd or Even1" returns true
+
   it('returns true for "mom"', () => {
     expect(palindromeChecker('mom')).toBe(true);
+  });
+
+  it('returns true for "Mom"', () => {
+    expect(palindromeChecker('Mom')).toBe(true);
+  });
+
+  it('returns true for "MoM"', () => {
+    expect(palindromeChecker('MoM')).toBe(true);
   });
   
 });
