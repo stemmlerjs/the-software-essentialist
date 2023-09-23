10d599cf51e59d8a9c2190a2acef3f343f4603f6
Barys Marukhin <barys_marukhin@epam.com>
2023-09-24 00:09:19 +0400

    [Green] returns 'Fizz' for 3

---
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -0,0 +1,3 @@
+export function fizzbuzz(number: number) {
+    return 'Fizz';
+}
--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,7 @@
+import { fizzbuzz } from './fizzbuzz';
 
 describe("fizzbuzz", () => {
-
-
+    it('returns "Fizz" for 3', () => {
+        expect(fizzbuzz(3)).toBe('Fizz');
+    });
 });
