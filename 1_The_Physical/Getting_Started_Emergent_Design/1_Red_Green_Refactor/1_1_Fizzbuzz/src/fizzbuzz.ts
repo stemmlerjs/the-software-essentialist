a7d9563d3ac7b1b69c2f606f42d664346b01648e
Barys Marukhin <barys_marukhin@epam.com>
2023-09-24 14:31:11 +0400

    [Green] throws errors for too small and too big numbers

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -2,7 +2,12 @@ export function fizzbuzz(number: number) {
     const isAMultipleOfThree = number % 3 === 0;
     const isAMultipleOfFive = number % 5 === 0;
 
+    if(number < 1) throw new Error('Too small');
+    if(number > 100) throw new Error('Too big');
+
     if (isAMultipleOfFive && isAMultipleOfThree) return'FizzBuzz';
     if (isAMultipleOfFive) return 'Buzz';
     if (isAMultipleOfThree) return 'Fizz';
+
+    return number.toString();
 }
--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,33 +1,42 @@
 import { fizzbuzz } from './fizzbuzz';
 
+// - It outputs numbers as strings
+// - It accepts numbers between 1 and 100
+// - It rejects numbers lower than 1
+// - It rejects numbers greater than 100
+
 /*
-* each return value is a string
-3 returns "Fizz"
-5 returns "Buzz
-15 returns "FizzBuzz"
-9 returns "Fizz"
-43 returns "43"
-42 returns "Fizz"
-45 returns "FizzBuzz"
-102 (you decide, throw an Error or handle some other way)
--12 (you decide, throw an Error or handle some other way)
-any non-number (you decide, throw an Error or handle some other way)
+- * each return value is a string
+- 3 returns "Fizz"
+- 5 returns "Buzz
+- 15 returns "FizzBuzz"
+- 9 returns "Fizz"
+- 43 returns "43"
+- 42 returns "Fizz"
+- 45 returns "FizzBuzz"
+- 102 (you decide, throw an Error or handle some other way)
+- -12 (you decide, throw an Error or handle some other way)
+- any non-number (you decide, throw an Error or handle some other way)
 * */
 
 describe("fizzbuzz", () => {
-    it('returns "Fizz" for 3', () => {
-        expect(fizzbuzz(3)).toBe('Fizz');
+    it.each([3, 6, 9])('returns "Fizz" for %s', (num) => {
+        expect(fizzbuzz(num)).toBe('Fizz');
     });
 
-    it('returns "Buzz" for 5', () => {
+    it.each([5, 10, 20])('returns "Buzz" for %s', () => {
         expect(fizzbuzz(5)).toBe('Buzz');
     });
 
-    it('returns "FizzBuzz" for 15', () => {
-        expect(fizzbuzz(15)).toBe('FizzBuzz');
+    it.each([15, 45])('returns "FizzBuzz" for %s', (num) => {
+        expect(fizzbuzz(num)).toBe('FizzBuzz');
     });
 
-    it.each([3, 6, 9])('returns "Fizz" for %#', () => {
-        expect(fizzbuzz(9)).toBe('Fizz');
+    it.each([0, -1, -12])('throws an error for too small number %s', (num) => {
+        expect(() => fizzbuzz(num)).toThrowError('Too small');
+    });
+
+    it.each([101, 102])('throws an error for too big number %s', (num) => {
+        expect(() => fizzbuzz(num)).toThrowError('Too big');
     });
 });
