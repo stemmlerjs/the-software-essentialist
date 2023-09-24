b237faa3ef0c84da364d393e066e5af228eb52d5
Barys Marukhin <barys_marukhin@epam.com>
2023-09-24 13:28:38 +0400

    [Green] returns 'Buzz' for 5

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,3 +1,3 @@
 export function fizzbuzz(number: number) {
-    return 'Fizz';
+    return number === 3 ? 'Fizz' : 'Buzz';
 }
--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,7 +1,25 @@
 import { fizzbuzz } from './fizzbuzz';
 
+/*
+* each return value is a string
+3 returns "Fizz"
+5 returns "Buzz
+15 returns "FizzBuzz"
+9 returns "Fizz"
+43 returns "43"
+42 returns "Fizz"
+45 returns "FizzBuzz"
+102 (you decide, throw an Error or handle some other way)
+-12 (you decide, throw an Error or handle some other way)
+any non-number (you decide, throw an Error or handle some other way)
+* */
+
 describe("fizzbuzz", () => {
     it('returns "Fizz" for 3', () => {
         expect(fizzbuzz(3)).toBe('Fizz');
+    });
+
+    it('returns "Buzz" for 5', () => {
+        expect(fizzbuzz(5)).toBe('Buzz');
     });
 });
