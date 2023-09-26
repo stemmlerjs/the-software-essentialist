b29c92ecc1bc6fe3335554386b77deae080cce28
Barys Marukhin <barys_marukhin@epam.com>
2023-09-26 12:42:26 +0400

    [Refactor] adds withoutSpaces and getReversed

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,4 +1,7 @@
-export const isPalindrome = (str: string) => {
-    const lowerNoSpacedStr = str.toLowerCase().split(' ').join('');
-    return lowerNoSpacedStr === lowerNoSpacedStr.split('').reverse().join('')
+const withoutSpaces = (str: string): string => str.indexOf(' ') === -1 ? str : withoutSpaces(str.replace(' ', ''));
+
+const getReversed = (str: string) => str.split('').reverse().join('')
+export const isPalindrome = (input: string) => {
+    const formattedInput = withoutSpaces(input.toLowerCase());
+    return formattedInput === getReversed(formattedInput);
 };
