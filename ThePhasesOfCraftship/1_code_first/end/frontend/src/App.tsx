d9677edecafbe0b0a40763bdd847ba61073f99a2
Khalil Stemmler <metroidman12@gmail.com>
2023-11-22 18:40:39 -0500

    initial app structure.

--- ThePhasesOfCraftship/1_code_first/end/frontend/src/App.tsx
+++ ThePhasesOfCraftship/1_code_first/end/frontend/src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Link } from "react-router-dom";
+
+const Header = ({ children }: any) => <header>{children}</header>;
+const Logo = () => (
+  <div>
+    <img src="https://em-content.zobj.net/source/twitter/376/brick_1f9f1.png"></img>
+  </div>
+);
+const TitleAndSubmission = () => (
+  <div>
+    <h1>Domain-Driven Designers</h1>
+    <h3>Where awesome domain driven designers are made</h3>
+    <Link to={"/submit"}>submit</Link>
+  </div>
+);
+const Join = () => (
+  <div>
+    <Link to="/join">Join</Link>
+  </div>
+);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Header>
+        <Logo />
+        <TitleAndSubmission />
+        <Join />
+      </Header>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
