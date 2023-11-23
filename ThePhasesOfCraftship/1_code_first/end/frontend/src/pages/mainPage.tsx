723571a80fc5d976de7bdb38a8004c5a30638dee
Khalil Stemmler <metroidman12@gmail.com>
2023-11-23 16:48:31 -0500

    Setup registration form deets.

--- ThePhasesOfCraftship/1_code_first/end/frontend/src/pages/mainPage.tsx
+++ ThePhasesOfCraftship/1_code_first/end/frontend/src/pages/mainPage.tsx
@@ -1,9 +1,43 @@
 
-import React from 'react';
-import { Layout } from '../components/layout';
+import React, { useState } from "react";
+import { Layout } from "../components/layout";
+import { PostsList } from "../components/postsList";
+import { PostsViewSwitcher } from "../components/postsViewSwitcher";
 
-export const MainPage = () => (
-  <Layout>
-    main page
-  </Layout>
-)
+const createRelativeDateString = (daysAgo: number) => {
+  let baseDate = new Date();
+  return "2 days ago";
+};
+
+export const MainPage = () => {
+  return (
+    <Layout>
+      <PostsViewSwitcher />
+      <PostsList
+        posts={[
+          {
+            title: "Domain Services vs. application services",
+            dateCreated: createRelativeDateString(2),
+            memberPostedBy: "stemmlerjs",
+            totalNumComments: 3,
+            voteCount: 5,
+          },
+          {
+            title: "Domain Services vs. application services",
+            dateCreated: createRelativeDateString(2),
+            memberPostedBy: "stemmlerjs",
+            totalNumComments: 3,
+            voteCount: 5,
+          },
+          {
+            title: "Domain Services vs. application services",
+            dateCreated: createRelativeDateString(2),
+            memberPostedBy: "stemmlerjs",
+            totalNumComments: 3,
+            voteCount: 5,
+          },
+        ]}
+      />
+    </Layout>
+  );
+};
