dd7f4346e4e92ed27486761696ebf1561cd720db
Khalil Stemmler <metroidman12@gmail.com>
2023-11-23 18:35:25 -0500

    Wrote all frontend registration code.

--- ThePhasesOfCraftship/1_code_first/end/frontend/src/api.tsx
+++ ThePhasesOfCraftship/1_code_first/end/frontend/src/api.tsx
@@ -1,6 +1,10 @@
+import { RegistrationInput } from "./components/registrationForm";
+import axios from 'axios'
 
 export const api = {
-  register: (username: string, email: string, password: string) => {
-    
+  register: (input: RegistrationInput) => {
+    return axios.post('http://localhost:3000/api/users/register', {
+      data: input
+    })
   }
 }
