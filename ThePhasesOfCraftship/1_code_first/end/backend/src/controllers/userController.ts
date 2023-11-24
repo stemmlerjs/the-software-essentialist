ef7deb3a65c766e7b0d52b945d7bfdc62d5bcbdf
Khalil Stemmler <metroidman12@gmail.com>
2023-11-24 12:57:59 -0500

    Wrote the posts, votes, comments, member prisma schema structure.

--- ThePhasesOfCraftship/1_code_first/end/backend/src/controllers/userController.ts
+++ ThePhasesOfCraftship/1_code_first/end/backend/src/controllers/userController.ts
@@ -1,6 +1,4 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import { prisma } from "../database";
 
 export async function createUser(userData: any) {
   try {
