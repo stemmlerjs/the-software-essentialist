6147dc1505f0e774096165d287e45bfbf43b08fb
Khalil Stemmler <metroidman12@gmail.com>
2023-11-27 17:26:42 -0500

    Starting off from where we left off in code-first.

---
+++ ThePhasesOfCraftship/2_best_practice_first/assignment/end/packages/backend/src/index.ts
@@ -0,0 +1,63 @@
+
+import express, { Request, Response } from 'express';
+import { createUser, editUser, getUserByEmail } from './controllers/userController';
+import { getRecentPosts } from './controllers/postsController';
+const cors = require('cors')
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+// Create a new user
+app.post('/users/new', async (req: Request, res: Response) => {
+  try {
+    console.log()
+    const user = await createUser(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to create user.' });
+  }
+});
+
+// Edit a user
+app.post('/users/edit/:userId', async (req: Request, res: Response) => {
+  try {
+    const user = await editUser(Number(req.params.userId), req.body);
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to edit user.' });
+  }
+});
+
+// Get a user by email
+app.get('/users', async (req: Request, res: Response) => {
+  try {
+    const email = req.query.email as string;
+    const user = await getUserByEmail(email);
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch user.' });
+  }
+});
+
+// Get a user by email
+app.get('/posts', async (req: Request, res: Response) => {
+  try {
+    const { sort } = req.query;
+    let posts;
+
+    if (sort === 'recent') {
+      posts = await getRecentPosts()
+    } else {
+      return res.status(500).json({ error: 'include ?sort= query'})
+    }
+
+    return res.json({ posts });
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching posts' });
+  }
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
