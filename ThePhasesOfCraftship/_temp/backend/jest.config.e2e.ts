3414747a499c064f78dad359830e0bceaea64da3
Khalil Stemmler <metroidman12@gmail.com>
2023-12-14 11:14:41 -0500

    Prepare for merge to main.

--- ThePhasesOfCraftship/_temp/backend/jest.config.e2e.ts
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.ts?$': 'ts-jest',
-  },
-  testEnvironment: 'node',
-  testRegex: './tests/.*\\.(e2e)?\\.(ts|ts)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  roots: ['<rootDir>/tests'],
-};
