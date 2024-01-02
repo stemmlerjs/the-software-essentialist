09b7406ba797969897ee0df4ef682f39901e2cd5
prosper20 <bobsonconnect@gmail.com>
2024-01-02 10:07:08 +0100

    [NEW]: fizzbuzz 15 returns 'FizzBuzz'

--- ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,4 +1,5 @@
 export function fizzBuzz(number: number) {
-  if (number % 3 === 0) return "Fizz";
-  else return "Buzz";
+  if (number % 3 === 0 && number % 5 === 0) return "FizzBuzz";
+  else if (number % 3 === 0) return "Fizz";
+  else if (number % 5 === 0) return "Buzz";
 }
--- ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -12,4 +12,8 @@ describe("fizzbuzz", () => {
   it('5 returns "Buzz"', () => {
     expect(fizzBuzz(5)).toEqual("Buzz");
   });
+
+  it('15 returns "FizzBuzz"', () => {
+    expect(fizzBuzz(15)).toEqual("FizzBuzz");
+  });
 });
