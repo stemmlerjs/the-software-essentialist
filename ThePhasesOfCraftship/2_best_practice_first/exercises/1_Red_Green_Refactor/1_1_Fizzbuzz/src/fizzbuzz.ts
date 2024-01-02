d51dbc122da136aaa06a7a698ec88741c4f909a1
prosper20 <bobsonconnect@gmail.com>
2024-01-02 09:29:29 +0100

    [NEW]: fizzbuzz retuens a string

---
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -0,0 +1,3 @@
+export function fizzBuzz(number: number) {
+  return "";
+}
--- ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,7 @@
+import { fizzBuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-
-
+  it("returns a string", () => {
+    expect(typeof fizzBuzz(5)).toBe("string");
+  });
 });
