e3c7800d530c29688cabaa30d3079ded8e34fb00
prosper20 <bobsonconnect@gmail.com>
2024-01-06 15:32:28 +0100

    [Refactor]: fizzbuzz introduce abstractions

--- ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,9 +1,12 @@
 export function fizzBuzz(number: number) {
+  const isMultipleOfThree = number % 3 === 0;
+  const isMultipleOfFive = number % 5 === 0;
+
   if (typeof number != "number") throw new Error("Input must  be a number");
   else if (number < 1) throw new Error("Number must not be less than 1");
   else if (number > 100) throw new Error("Number must not be above 100");
-  else if (number % 3 === 0 && number % 5 === 0) return "FizzBuzz";
-  else if (number % 3 === 0) return "Fizz";
-  else if (number % 5 === 0) return "Buzz";
+  else if (isMultipleOfThree && isMultipleOfFive) return "FizzBuzz";
+  else if (isMultipleOfThree) return "Fizz";
+  else if (isMultipleOfFive) return "Buzz";
   else return number.toString();
 }
