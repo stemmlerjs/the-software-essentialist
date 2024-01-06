033f34900a5594dc4543a0433245381789d4d3b8
prosper20 <bobsonconnect@gmail.com>
2024-01-06 15:52:51 +0100

    [Refactor]: removes else statements

--- ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -3,10 +3,10 @@ export function fizzBuzz(number: number) {
   const isMultipleOfFive = number % 5 === 0;
 
   if (typeof number != "number") throw new Error("Input must  be a number");
-  else if (number < 1) throw new Error("Number must not be less than 1");
-  else if (number > 100) throw new Error("Number must not be above 100");
-  else if (isMultipleOfThree && isMultipleOfFive) return "FizzBuzz";
-  else if (isMultipleOfThree) return "Fizz";
-  else if (isMultipleOfFive) return "Buzz";
-  else return number.toString();
+  if (number < 1) throw new Error("Number must not be less than 1");
+  if (number > 100) throw new Error("Number must not be above 100");
+  if (isMultipleOfThree && isMultipleOfFive) return "FizzBuzz";
+  if (isMultipleOfThree) return "Fizz";
+  if (isMultipleOfFive) return "Buzz";
+  return number.toString();
 }
