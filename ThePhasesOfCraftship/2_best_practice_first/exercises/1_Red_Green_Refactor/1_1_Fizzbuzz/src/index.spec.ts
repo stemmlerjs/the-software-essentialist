38db3fee7ba7368ee63e4175dbf9a8796402d416
Tiago <tiagojacintodev@gmail.com>
2024-03-16 14:42:12 +0000

    [refactor]: reduce duplication by using it.each function

--- ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -7,71 +7,24 @@ describe('fizzbuzz', () => {
     fizzbuzz = new FizzBuzz();
   });
 
-  it('should not be able to execute on -5', () => {
-    const number = -5;
-
+  it.each([-5, 0, 101])('should not be able to execute on %i', (number) => {
     expect(() => fizzbuzz.execute(number)).toThrow();
   });
 
-  it('should not be able to execute on 0', () => {
-    const number = 0;
+  it.each`
+    number | expected
+    ${1}   | ${'1'}
+    ${2}   | ${'12'}
+    ${3}   | ${'12Fizz'}
+    ${5}   | ${'12Fizz4Buzz'}
+    ${15}  | ${'12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz'}
+    ${30}  | ${'12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz1617Fizz19BuzzFizz2223FizzBuzz26Fizz2829FizzBuzz'}
+  `(
+    'should be able to output "$expected" on number $number',
+    ({ number, expected }) => {
+      const result = fizzbuzz.execute(number);
 
-    expect(() => fizzbuzz.execute(number)).toThrow();
-  });
-
-  it('should not be able to execute on 101', () => {
-    const number = 101;
-
-    expect(() => fizzbuzz.execute(number)).toThrow();
-  });
-
-  it('should be able to output "1" on number 1', () => {
-    const number = 1;
-
-    const result = fizzbuzz.execute(number);
-
-    expect(result).toBe('1');
-  });
-
-  it('should be able to output "12" on number 2', () => {
-    const number = 2;
-
-    const result = fizzbuzz.execute(number);
-
-    expect(result).toBe('12');
-  });
-
-  it('should be able to output "12Fizz" on number 3', () => {
-    const number = 3;
-
-    const result = fizzbuzz.execute(number);
-
-    expect(result).toBe('12Fizz');
-  });
-
-  it('should be able to output "12Fizz4Buzz" on number 5', () => {
-    const number = 5;
-
-    const result = fizzbuzz.execute(number);
-
-    expect(result).toBe('12Fizz4Buzz');
-  });
-
-  it('should be able to output "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz" on number 15', () => {
-    const number = 15;
-
-    const result = fizzbuzz.execute(number);
-
-    expect(result).toBe('12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz');
-  });
-
-  it('should be able to output "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz1617Fizz19BuzzFizz2223FizzBuzz26Fizz2829FizzBuzz" on number 30', () => {
-    const number = 30;
-
-    const result = fizzbuzz.execute(number);
-
-    expect(result).toBe(
-      '12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz1617Fizz19BuzzFizz2223FizzBuzz26Fizz2829FizzBuzz'
-    );
-  });
+      expect(result).toBe(expected);
+    }
+  );
 });
