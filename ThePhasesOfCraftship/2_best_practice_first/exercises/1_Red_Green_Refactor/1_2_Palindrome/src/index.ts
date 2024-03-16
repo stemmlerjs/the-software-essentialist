af307b3be7a83bc9ceca790dfb125514a32a5f69
Tiago <tiagojacintodev@gmail.com>
2024-03-16 16:40:24 +0000

    [refactor]: move reverse string logic to a separate function

--- ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,6 +1,11 @@
+function reverseString(str: string) {
+  return str.split('').reverse().join('');
+}
+
 export class PalindromeChecker {
   isAPalindrome(str: string): boolean {
-    const reversed = str.split('').reverse().join('');
+    const reversed = reverseString(str);
+
     if (
       str.toLowerCase().split(' ').join('') !==
       reversed.toLowerCase().split(' ').join('')
