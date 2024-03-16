4a99365e0c5954c63f0c15a4acaa32a533a4ef7e
Tiago <tiagojacintodev@gmail.com>
2024-03-16 13:38:21 +0000

    [green]: -5 number input throws

---
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -0,0 +1,6 @@
+export class FizzBuzz {
+  execute(number: number): string {
+    if (number === -5) throw new Error();
+    return '';
+  }
+}
--- ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,9 +1,12 @@
+import { FizzBuzz } from './fizzbuzz';
+
 describe('fizzbuzz', () => {
   it('should not be able to execute on -5', () => {
     // given
+    const fizzbuzz = new FizzBuzz();
     const number = -5;
 
     // when / then
-    expect(fizzbuzz.execute(number)).toThrow();
+    expect(() => fizzbuzz.execute(number)).toThrow();
   });
 });
