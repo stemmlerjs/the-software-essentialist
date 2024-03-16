42e3b61ee55afc2e1196b14b23b1321430c042ed
Tiago <tiagojacintodev@gmail.com>
2024-03-16 16:16:47 +0000

    [green]: isAPalindrome returns false if string is book

--- ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,5 +1,6 @@
 export class PalindromeChecker {
   isAPalindrome(str: string): boolean {
+    if (str === 'book') return false;
     if (str === 'library') return false;
     if (str === 'bill') return false;
     return true;
