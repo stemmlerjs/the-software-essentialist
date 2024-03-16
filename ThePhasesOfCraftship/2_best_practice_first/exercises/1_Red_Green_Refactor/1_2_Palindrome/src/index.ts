fb672661ac2ded597336b15aaf3f35c41d8b8055
Tiago <tiagojacintodev@gmail.com>
2024-03-16 16:41:52 +0000

    [refactor]: move strip spaces from string logic to separate function

--- ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -2,14 +2,15 @@ function reverseString(str: string) {
   return str.split('').reverse().join('');
 }
 
+function stripSpaces(str: string) {
+  return str.split(' ').join('');
+}
+
 export class PalindromeChecker {
   isAPalindrome(str: string): boolean {
     const reversed = reverseString(str);
 
-    if (
-      str.toLowerCase().split(' ').join('') !==
-      reversed.toLowerCase().split(' ').join('')
-    )
+    if (stripSpaces(str.toLowerCase()) !== stripSpaces(reversed.toLowerCase()))
       return false;
     return true;
   }
