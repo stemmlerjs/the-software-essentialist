debcb52867d8f868eeaab16b6081700c981940fd
Tiago <tiagojacintodev@gmail.com>
2024-03-16 16:22:53 +0000

    [green]: isAPalindrome returns false if insensitive cased string and insensitive cased reversed string are not equal

--- ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,7 +1,7 @@
 export class PalindromeChecker {
   isAPalindrome(str: string): boolean {
     const reversed = str.split('').reverse().join('');
-    if (str !== reversed) return false;
+    if (str.toLowerCase() !== reversed.toLowerCase()) return false;
     return true;
   }
 }
