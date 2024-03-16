cd503e3b9163fff7ce8820261aea8d49fc63c131
Tiago <tiagojacintodev@gmail.com>
2024-03-16 16:33:51 +0000

    [refactor]: define parameterized test on valid word palindromes even if casing is off

--- ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -7,8 +7,17 @@ describe('palindrome checker', () => {
     palindromeChecker = new PalindromeChecker();
   });
 
-  it.each(['mom', 'wow', 'Mom'])(
+  it.each(['mom', 'wow'])(
     'should be able to verify that the word "%s" is a valid palindrome',
+    (word) => {
+      const result = palindromeChecker.isAPalindrome(word);
+
+      expect(result).toBeTruthy();
+    }
+  );
+
+  it.each(['Mom', 'DAD', 'WoW'])(
+    'should be able to verify that the word "%s" with is a valid palindrome even if casing is off',
     (word) => {
       const result = palindromeChecker.isAPalindrome(word);
 
