004791cfe94bacf53c3b566b504771faad31f967
Tiago <tiagojacintodev@gmail.com>
2024-03-18 17:52:52 +0000

    [green]: knows that the numbers [0,1,2] have a minimum value of 0

--- ThePhasesOfCraftship/2_best_practice_first/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -8,4 +8,12 @@ describe('stats calculator', () => {
 
     expect(result.minimum).toBe(-8);
   });
+
+  it('knows that the numbers [0,1,2] have a minimum value of 0', () => {
+    const integers = [0, 1, 2];
+
+    const result = StatsCalculator.calculate(integers);
+
+    expect(result.minimum).toBe(0);
+  });
 });
--- ThePhasesOfCraftship/2_best_practice_first/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.ts
@@ -1,7 +1,13 @@
 export class StatsCalculator {
   static calculate(numbers: number[]) {
+    let minimum = numbers[0];
+
+    for (let i = 1; i < numbers.length; i++) {
+      if (numbers[i] < minimum) minimum = numbers[i];
+    }
+
     return {
-      minimum: -8,
+      minimum,
     };
   }
 }
