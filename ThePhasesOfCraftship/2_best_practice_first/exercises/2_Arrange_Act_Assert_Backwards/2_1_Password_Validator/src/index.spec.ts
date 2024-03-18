276818c3913ad8b136b64ff82f5f761c32deb973
Tiago <tiagojacintodev@gmail.com>
2024-03-18 12:32:14 +0000

    knows that a password that is outside 5 and 15 characters long is invalid

--- ThePhasesOfCraftship/2_best_practice_first/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -12,4 +12,20 @@ describe('password validator', () => {
     // Assert
     expect(result.result).toBeTruthy();
   });
+
+  it('knows that a password that is outside 5 and 15 characters long is invalid', () => {
+    // Arrange
+    const password = 'mom';
+    const sut = new PasswordValidator();
+
+    // Act
+    const result = sut.validatePassword(password);
+
+    // Assert
+    expect(result.result).toBeFalsy();
+    expect(result.errors).toBeDefined();
+    expect(result.errors!.length).toBe(1);
+    expect(result.errors![0].type).toBe('IncorrectPasswordLength');
+    expect(result.errors![0].message).toContain('5 and 15');
+  });
 });
--- ThePhasesOfCraftship/2_best_practice_first/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
@@ -1,5 +1,17 @@
 export class PasswordValidator {
   validatePassword(password: string) {
+    if (password.length < 5 || password.length > 15) {
+      return {
+        result: false,
+        errors: [
+          {
+            type: 'IncorrectPasswordLength',
+            message: 'Password must contain between 5 and 15 characters',
+          },
+        ],
+      };
+    }
+
     return {
       result: true,
     };
