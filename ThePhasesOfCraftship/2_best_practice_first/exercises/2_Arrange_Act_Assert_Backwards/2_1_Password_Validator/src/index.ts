8c619be2dc0c8ef6a9f6bc5e40bf5baa1a4b58f0
Tiago <tiagojacintodev@gmail.com>
2024-03-18 14:12:25 +0000

    [refactor]: separate regex logic into separate functions and define min and max length variables

--- ThePhasesOfCraftship/2_best_practice_first/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
@@ -7,21 +7,24 @@ export class PasswordValidator {
   validatePassword(password: string) {
     let errors: ValidationError[] = [];
 
-    if (password.length < 5 || password.length > 15) {
+    const MIN_LENGTH = 5;
+    const MAX_LENGTH = 15;
+
+    if (password.length < MIN_LENGTH || password.length > MAX_LENGTH) {
       errors.push({
         type: 'IncorrectPasswordLength',
         message: 'Password must contain between 5 and 15 characters',
       });
     }
 
-    if (!/\d/.test(password)) {
+    if (!hasAtLeast1Digit(password)) {
       errors.push({
         type: 'PasswordMustHaveAtLeastOneDigit',
         message: 'The password must have at least 1 digit',
       });
     }
 
-    if (!/[A-Z]/.test(password)) {
+    if (!hasAtLeast1UpperCase(password)) {
       errors.push({
         type: 'PasswordMustHaveAtLeastOneUpperCaseLetter',
         message: 'The password must have at least 1 upper case letter',
@@ -39,4 +42,12 @@ export class PasswordValidator {
           result: true,
         };
   }
+}
+
+function hasAtLeast1Digit(str: string) {
+  return /\d/.test(str);
+}
+
+function hasAtLeast1UpperCase(str: string) {
+  return /[A-Z]/.test(str);
 }
