1e91b9376c099f111286905887ffa476bcef6ca1
Tiago <tiagojacintodev@gmail.com>
2024-03-18 14:06:18 +0000

    [green]: change tests to verify only the specific error

--- ThePhasesOfCraftship/2_best_practice_first/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -8,78 +8,103 @@ describe('password validator', () => {
   });
 
   describe('password length must be between 5 and 15 characters long', () => {
-    it.each(['Password1', 'Test1', 'Length7'])(
+    it.each(['password', 'qwerty', '123456'])(
       'knows that "%s" has valid length',
       (password) => {
         const result = sut.validatePassword(password);
 
-        expect(result.result).toBeTruthy();
-        expect(result.errors).not.toBeDefined();
+        expect(
+          result.errors?.find((err) => err.type === 'IncorrectPasswordLength')
+        ).not.toBeDefined();
       }
     );
 
-    it.each(['Mom1', 'T123', 'PT1'])(
+    it.each(['mom', 'test', 'pt'])(
       'knows that "%s" has invalid length',
       (password) => {
         const { result, errors } = sut.validatePassword(password);
 
         expect(result).toBeFalsy();
         expect(errors).toBeDefined();
-        expect(errors!.length).toBe(1);
-        expect(errors![0].type).toBe('IncorrectPasswordLength');
-        expect(errors![0].message).toContain('5 and 15');
+
+        expect(
+          errors?.find((err) => err.type === 'IncorrectPasswordLength')
+        ).toBeDefined();
+
+        expect(
+          errors?.find((err) => err.type === 'IncorrectPasswordLength')?.message
+        ).toContain('5 and 15');
       }
     );
   });
 
   describe('password must have at least one digit', () => {
-    it.each(['Password1', 'Has1Digit', 'Has2Digits'])(
+    it.each(['digit1', '1', '123'])(
       'knows that "%s" has at least one digit',
       (password) => {
         const result = sut.validatePassword(password);
 
-        expect(result.result).toBeTruthy();
-        expect(result.errors).not.toBeDefined();
+        expect(
+          result.errors?.find(
+            (err) => err.type === 'PasswordMustHaveAtLeastOneDigit'
+          )
+        ).not.toBeDefined();
       }
     );
 
-    it.each(['Password', 'HasNoDigits', 'HasZeroDigits'])(
+    it.each(['password', 'HasNoDigits', 'HasZeroDigits'])(
       'knows that "%s" doesn\'t have at least one digit',
       (password) => {
         const { result, errors } = sut.validatePassword(password);
 
         expect(result).toBeFalsy();
-        expect(errors!).toBeDefined();
-        expect(errors!.length).toBe(1);
-        expect(errors![0].type).toBe('PasswordMustHaveAtLeastOneDigit');
-        expect(errors![0].message).toContain('at least 1 digit');
+        expect(errors).toBeDefined();
+
+        expect(
+          errors?.find((err) => err.type === 'PasswordMustHaveAtLeastOneDigit')
+        ).toBeDefined();
+
+        expect(
+          errors?.find((err) => err.type === 'PasswordMustHaveAtLeastOneDigit')
+            ?.message
+        ).toContain('at least 1 digit');
       }
     );
   });
 
   describe('password must have at least one upper case letter', () => {
-    it.each(['Password1', 'Has1UpperCase', 'Has2UpperCase'])(
+    it.each(['U', 'UPPER', 'HasMultipleUpperCases'])(
       'knows that "%s" has at least one upper case letter',
       (password) => {
         const result = sut.validatePassword(password);
 
-        expect(result.result).toBeTruthy();
-        expect(result.errors).not.toBeDefined();
+        expect(
+          result.errors?.find(
+            (err) => err.type === 'PasswordMustHaveAtLeastOneUpperCaseLetter'
+          )
+        ).not.toBeDefined();
       }
     );
 
-    it.each(['password2', 'nouppercase1', 'has0uppercase'])(
+    it.each(['u', 'lower', 'nouppercase'])(
       'knows that "%s" doesn\'t have at least one upper case letter',
       (password) => {
         const { result, errors } = sut.validatePassword(password);
 
         expect(result).toBeFalsy();
-        expect(errors!).toBeDefined();
-        expect(errors!.length).toBe(1);
-        expect(errors![0].type).toBe(
-          'PasswordMustHaveAtLeastOneUpperCaseLetter'
-        );
-        expect(errors![0].message).toContain('at least 1 upper case letter');
+        expect(errors).toBeDefined();
+
+        expect(
+          errors?.find(
+            (err) => err.type === 'PasswordMustHaveAtLeastOneUpperCaseLetter'
+          )
+        ).toBeDefined();
+
+        expect(
+          errors?.find(
+            (err) => err.type === 'PasswordMustHaveAtLeastOneUpperCaseLetter'
+          )?.message
+        ).toContain('at least 1 upper case letter');
       }
     );
   });
