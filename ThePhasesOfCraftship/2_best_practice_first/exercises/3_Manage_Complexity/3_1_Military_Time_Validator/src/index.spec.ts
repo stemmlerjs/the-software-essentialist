269e7fd263ec50ac0b9911eca7ead4b90f5c674e
Tiago <tiagojacintodev@gmail.com>
2024-03-19 12:05:28 +0000

    [New]: add tests cases to invalid time ranges

--- ThePhasesOfCraftship/2_best_practice_first/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -9,12 +9,15 @@ describe('military time range validator', () => {
     expect(result).toBe(true);
   });
 
-  it.each(['00:00 23:59', '00:56 21:59', '10:50 20:36'])(
-    'knows that "00:00 23:59" is an invalid time range',
-    (range) => {
-      const result = MilitaryTimeRangeValidator.validate(range);
+  it.each([
+    '00:00 23:59',
+    '00:56 21:59',
+    '10:50 20:36',
+    '10:50 -- 20:36',
+    '10:50 - - 20:36',
+  ])('knows that "00:00 23:59" is an invalid time range', (range) => {
+    const result = MilitaryTimeRangeValidator.validate(range);
 
-      expect(result).toBe(false);
-    }
-  );
+    expect(result).toBe(false);
+  });
 });
--- ThePhasesOfCraftship/2_best_practice_first/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,6 +1,6 @@
 export class MilitaryTimeRangeValidator {
   static validate(range: string): boolean {
-    if (!range.includes('-')) return false;
+    if (range.split('-').length !== 2) return false;
     return true;
   }
 }
