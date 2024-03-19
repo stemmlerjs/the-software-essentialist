345a2c891cf3ae7a25596b10f623b2fd1eb9972b
Tiago <tiagojacintodev@gmail.com>
2024-03-19 12:17:38 +0000

    [New]: add tests cases for checking that the range is separated by only one separator

--- ThePhasesOfCraftship/2_best_practice_first/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,23 +1,21 @@
 import { MilitaryTimeRangeValidator } from './index';
 
 describe('military time range validator', () => {
-  it('knows that "00:00 - 23:59" is a valid time range', () => {
-    const range = '00:00 - 23:59';
+  describe('range has a "-" separator', () => {
+    it.each([
+      ['03:03-23:59', true],
+      ['00:00 - 23:59', true],
+      ['04:52 - 21:39', true],
+      ['20:04 - 12:35', true],
+      ['00:00 23:59', false],
+      ['00:56 21:59', false],
+      ['10:50 20:36', false],
+      ['10:50 -- 20:36', false],
+      ['10:50 - - 20:36', false],
+    ])('knows that "%s" is %s', (range, expected) => {
+      const result = MilitaryTimeRangeValidator.validate(range);
 
-    const result = MilitaryTimeRangeValidator.validate(range);
-
-    expect(result).toBe(true);
-  });
-
-  it.each([
-    '00:00 23:59',
-    '00:56 21:59',
-    '10:50 20:36',
-    '10:50 -- 20:36',
-    '10:50 - - 20:36',
-  ])('knows that "00:00 23:59" is an invalid time range', (range) => {
-    const result = MilitaryTimeRangeValidator.validate(range);
-
-    expect(result).toBe(false);
+      expect(result).toBe(expected);
+    });
   });
 });
