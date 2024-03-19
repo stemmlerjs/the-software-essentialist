b4203506bbbf2355f965b89f73a82bc2c822e0d4
Tiago <tiagojacintodev@gmail.com>
2024-03-19 15:07:29 +0000

    [Refactor]: move logic that isn't related to time validation to separate functions

--- ThePhasesOfCraftship/2_best_practice_first/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,23 +1,44 @@
 export class MilitaryTimeRangeValidator {
   static validate(range: string): boolean {
-    const timestamps = range.split('-');
+    const timestamps = getTimestamps(range);
     if (timestamps.length !== 2) return false;
 
     for (const timestamp of timestamps) {
-      const sides = timestamp.trim().split(':');
+      const sides = getSides(timestamp) as [string, string];
       if (sides.length !== 2) return false;
 
-      for (const side of sides) {
-        if (!/^\d{2}$/.test(side)) return false;
-      }
+      if (!sides.every(has2Digits)) return false;
 
-      const hours = parseInt(sides[0]);
-      const minutes = parseInt(sides[1]);
+      const { hours, minutes } = getHoursAndMinutes(sides);
 
-      if (hours > 23) return false;
-      if (minutes > 59) return false;
+      const MAX_HOURS = 23;
+      const MAX_MINUTES = 59;
+
+      if (hours > MAX_HOURS) return false;
+      if (minutes > MAX_MINUTES) return false;
     }
 
     return true;
   }
+}
+
+function getTimestamps(range: string): string[] {
+  return range.split('-').map((timestamp) => timestamp.trim());
+}
+
+function getSides(timestamp: string): string[] {
+  return timestamp.split(':').map((side) => side.trim());
+}
+
+function has2Digits(str: string) {
+  return /^\d{2}$/.test(str);
+}
+
+function getHoursAndMinutes(sides: [string, string]) {
+  const [hours, minutes] = sides.map((side) => parseInt(side)) as [
+    number,
+    number
+  ];
+
+  return { hours, minutes };
 }
