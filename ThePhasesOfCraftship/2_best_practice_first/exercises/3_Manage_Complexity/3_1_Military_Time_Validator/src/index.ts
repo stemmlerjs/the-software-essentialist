dc7c77b27f17d7f8a1823a21c1f3942528a7664d
Tiago <tiagojacintodev@gmail.com>
2024-03-19 12:02:56 +0000

    [Refactor]: remove duplication

--- ThePhasesOfCraftship/2_best_practice_first/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,8 +1,6 @@
 export class MilitaryTimeRangeValidator {
   static validate(range: string): boolean {
-    if (range === '00:00 23:59') return false;
-    if (range === '00:56 21:59') return false;
-    if (range === '10:50 20:36') return false;
+    if (!range.includes('-')) return false;
     return true;
   }
 }
