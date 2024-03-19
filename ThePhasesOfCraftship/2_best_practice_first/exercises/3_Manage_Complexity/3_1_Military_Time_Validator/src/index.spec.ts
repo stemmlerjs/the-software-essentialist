9a936aab8ea2cbd628f65468c955853f5649dd21
Tiago <tiagojacintodev@gmail.com>
2024-03-19 12:00:43 +0000

    [New]: add tests cases to invalid time ranges

--- ThePhasesOfCraftship/2_best_practice_first/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -9,11 +9,12 @@ describe('military time range validator', () => {
     expect(result).toBe(true);
   });
 
-  it('knows that "00:00 23:59" is a invalid time range', () => {
-    const range = '00:00 23:59';
+  it.each(['00:00 23:59', '00:56 21:59', '10:50 20:36'])(
+    'knows that "00:00 23:59" is a invalid time range',
+    (range) => {
+      const result = MilitaryTimeRangeValidator.validate(range);
 
-    const result = MilitaryTimeRangeValidator.validate(range);
-
-    expect(result).toBe(false);
-  });
+      expect(result).toBe(false);
+    }
+  );
 });
--- ThePhasesOfCraftship/2_best_practice_first/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,6 +1,8 @@
 export class MilitaryTimeRangeValidator {
   static validate(range: string): boolean {
     if (range === '00:00 23:59') return false;
+    if (range === '00:56 21:59') return false;
+    if (range === '10:50 20:36') return false;
     return true;
   }
 }
