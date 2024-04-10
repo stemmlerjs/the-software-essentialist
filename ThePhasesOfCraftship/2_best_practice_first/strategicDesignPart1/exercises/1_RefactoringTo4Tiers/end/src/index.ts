1705481cd7f91de49fe78076d6541eeabc7111a3
Lucas Machado <lucasmachado.rj@gmail.com>
2024-04-10 02:48:11 -0300

    encapsulate express application

--- ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/end/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/end/src/index.ts
@@ -1,16 +1,5 @@
-import express from 'express';
-import { AssignmentsRouter, ClassesRouter, StudentsRouter } from './controllers';
-import { enableGracefulShutdown } from './shared/server';
+import application from "./application";
 
+const PORT = Number(process.env.PORT || 3000);
 
-const app = express();
-const port = process.env.PORT ?? 3000;
-
-app.use(express.json());
-app.use('/students', StudentsRouter); 
-app.use('/classes', ClassesRouter);
-app.use('/assignments', AssignmentsRouter);
-
-
-const server = app.listen(port, () => console.log(`Listening on port ${port}`));
-enableGracefulShutdown(server);
+application.start(PORT);
