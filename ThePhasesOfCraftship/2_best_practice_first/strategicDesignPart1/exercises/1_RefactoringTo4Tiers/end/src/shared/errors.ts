1f0ee4d9ad0188895c2ff26d6b947c372b8ac353
Lucas Machado <lucasmachado.rj@gmail.com>
2024-04-10 02:23:31 -0300

    add error message to error objects

--- ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/end/src/shared/errors.ts
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/end/src/shared/errors.ts
@@ -5,14 +5,14 @@ import Errors from "./constants";
 
 function errorHandler(error: Error, req: Request, res: Response, next: NextFunction) {
     if (error instanceof InvalidRequestBodyException) {
-        return res.status(400).json({ error: Errors.ValidationError, data: undefined, success: false });
+        return res.status(400).json({ error: Errors.ValidationError, data: undefined, success: false, message: error.message});
     }
 
     if (error instanceof StudentNotFoundException) {
-        return res.status(404).json({ error: Errors.StudentNotFound, data: undefined, success: false });
+        return res.status(404).json({ error: Errors.StudentNotFound, data: undefined, success: false, message: error.message });
     }
 
-    return res.status(500).json({ error: Errors.ServerError, data: undefined, success: false });
+    return res.status(500).json({ error: Errors.ServerError, data: undefined, success: false, message: error.message});
 }
 
 export {
