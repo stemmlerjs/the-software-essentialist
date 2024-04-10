d193cf84dcbff9406b82542a051d5bd3b64d6ab8
Lucas Machado <lucasmachado.rj@gmail.com>
2024-04-10 00:38:43 -0300

    expose the controller modules

---
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/end/src/controllers/index.ts
@@ -0,0 +1,10 @@
+import StudentsRouter from './students';
+import ClassesRouter from './classes';
+import AssignmentsRouter from './assignments';
+
+
+export {
+    StudentsRouter,
+    ClassesRouter,
+    AssignmentsRouter
+}
