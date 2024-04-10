625b580c6ec512688973ef3a1cfe66cc3828e3f3
Lucas Machado <lucasmachado.rj@gmail.com>
2024-04-10 00:34:28 -0300

    encapsulate errors on its own module

---
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/end/src/utils/errors.ts
@@ -0,0 +1,13 @@
+const Errors = {
+    ValidationError: 'ValidationError',
+    StudentNotFound: 'StudentNotFound',
+    ClassNotFound: 'ClassNotFound',
+    AssignmentNotFound: 'AssignmentNotFound',
+    ServerError: 'ServerError',
+    ClientError: 'ClientError',
+    StudentAlreadyEnrolled: 'StudentAlreadyEnrolled'
+}
+
+
+export default Errors;
+
