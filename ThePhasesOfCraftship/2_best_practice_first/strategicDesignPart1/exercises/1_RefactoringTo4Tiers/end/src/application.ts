65b947e6f68961a06ec749ba710d0560566daab8
Lucas Machado <lucasmachado.rj@gmail.com>
2024-04-10 02:48:11 -0300

    encapsulate express application

---
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/end/src/application.ts
@@ -0,0 +1,32 @@
+import express, { Application as ExpressApp } from 'express';
+import { AssignmentsRouter, ClassesRouter, StudentsRouter } from './controllers';
+import { enableGracefulShutdown } from './shared/server';
+
+class Application {
+    private readonly _instance: ExpressApp;
+
+    get instance(): ExpressApp {
+        return this._instance;
+    }
+    constructor() {
+        this._instance = express();
+        this.addMiddlewares();
+        this.registerRouters();
+    }
+    private addMiddlewares() {
+        this._instance.use(express.json());
+    }
+
+    private registerRouters() {
+        this._instance.use('/students', StudentsRouter); 
+        this._instance.use('/classes', ClassesRouter);
+        this._instance.use('/assignments', AssignmentsRouter);
+    }
+
+    public start(port: number) {
+        const server = this._instance.listen(port, () => console.log(`Listening on port ${port}`));
+        enableGracefulShutdown(server);
+    }
+}
+
+export default new Application();
