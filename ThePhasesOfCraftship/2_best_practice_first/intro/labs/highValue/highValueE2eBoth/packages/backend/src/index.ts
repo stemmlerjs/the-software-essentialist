90133f333c8f1f4a60484c54084df1e938494dbd
Khalil Stemmler <metroidman12@gmail.com>
2024-04-15 13:14:46 -0400

    restructured.

--- ThePhasesOfCraftship/2_best_practice_first/intro/labs/highValue/highValueE2eBoth/packages/backend/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { bootstrap } from "./shared/bootstrap";
-
-bootstrap();
--- ThePhasesOfCraftship/2_best_practice_first/intro/labs/highValue/packages/shared/src/api/posts.ts
+++ ThePhasesOfCraftship/2_best_practice_first/intro/labs/highValue/packages/shared/src/api/posts.ts
@@ -8,12 +8,16 @@ export type GetPostsQuery = {
   sort: GetPostsQueryOptions
 }
 
+export type GetPostsData = {
+
+}
+
 export const createPostsAPI = (apiURL: string) => {
   return {
-    getPosts: async (): Promise<APIResponse> => {
+    getPosts: async (): Promise<APIResponse<GetPostsData>> => {
       try {
         const successResponse = await axios.get(`${apiURL}/posts?sort=recent`)
-        return successResponse.data as APIResponse;
+        return successResponse.data as APIResponse<GetPostsData>;
       } catch (err) {
         //@ts-ignore
         return err.response.data as APIResponse;
