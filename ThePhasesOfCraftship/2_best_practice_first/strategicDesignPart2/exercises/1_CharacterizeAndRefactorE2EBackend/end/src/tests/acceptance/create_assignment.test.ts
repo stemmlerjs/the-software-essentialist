d495d3bc1b8a6dfddba7743f33188dca17bea7bd
Lucas Machado <lucasmachado.rj@gmail.com>
2024-04-16 18:18:00 -0300

    characterization test to create assignment use case

---
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart2/exercises/1_CharacterizeAndRefactorE2EBackend/end/src/tests/acceptance/create_assignment.test.ts
@@ -0,0 +1,50 @@
+import request from 'supertest';
+import { app } from '../../index';
+
+
+import { defineFeature, loadFeature } from 'jest-cucumber';
+import path from 'path';
+import { resetDatabase } from '../fixtures/reset';
+import { prisma } from '../../database';
+
+const feature = loadFeature(path.join(__dirname, '../features/create_assignment.feature'));
+
+defineFeature(feature, (test) => {
+    afterAll(async () => {
+        await resetDatabase();
+    })
+
+    test('Successfully create an assignment', ({ given, when, then, and }) => {
+        let requestBody: any = {};
+        let response: any = {};
+        let class_: any = null;
+
+        given(/^I give a class named "(.*)"$/, async (name) => {
+            class_ = await prisma.class.create({
+                data: {
+                    name
+                }
+            });
+        })
+
+        and(/^I want to create an assignment named "(.*)"$/, (title) => {
+            requestBody = {
+                title,
+                classId: class_.id
+            }
+        })
+
+        when('I send a request to create an assignment', async () => {
+            response = await request(app)
+                .post('/assignments')
+                .send(requestBody);
+
+            console.log(response.body)
+        });
+
+        then('the assignment should be created successfully', () => {
+            expect(response.status).toBe(201);
+            expect(response.body.data.name).toBe(requestBody.name);
+        });
+    })
+})
