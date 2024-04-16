1bc2a54af764d2ea4a2be8bda027b7604a880366
Lucas Machado <lucasmachado.rj@gmail.com>
2024-04-16 18:49:36 -0300

    run prettier

--- ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart2/exercises/1_CharacterizeAndRefactorE2EBackend/end/src/tests/acceptance/create_student.test.ts
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart2/exercises/1_CharacterizeAndRefactorE2EBackend/end/src/tests/acceptance/create_student.test.ts
@@ -1,67 +1,64 @@
-import request from 'supertest';
-import { app } from '../../index';
+import request from "supertest";
+import { app } from "../../index";
 
+import { defineFeature, loadFeature } from "jest-cucumber";
+import path from "path";
+import { resetDatabase } from "../fixtures/reset";
 
-import { defineFeature, loadFeature } from 'jest-cucumber';
-import path from 'path';
-import { resetDatabase } from '../fixtures/reset';
-
-const feature = loadFeature(path.join(__dirname, '../features/create_student.feature'));
+const feature = loadFeature(
+  path.join(__dirname, "../features/create_student.feature")
+);
 
 defineFeature(feature, (test) => {
+  afterAll(async () => {
+    await resetDatabase();
+  });
 
-    afterAll(async () => {
-        await resetDatabase();
-    })
+  test("Successful Student Creation", ({ given, when, then }) => {
+    let requestBody: any = {};
+    let response: any = {};
 
-    test('Successful Student Creation', ({ given, when, then }) => {
-        
-        let requestBody: any = {};
-        let response: any = {};
+    given(
+      /^I want to create a student named "(.*)" and with email "(.*)"$/,
+      (name, email) => {
+        requestBody = {
+          name,
+          email,
+        };
+      }
+    );
 
-        given(/^I want to create a student named "(.*)" and with email "(.*)"$/, (name, email) => {
-            requestBody = {
-                name,
-                email
-            }
-        })
+    when("I send a request to create a student", async () => {
+      response = await request(app).post("/students").send(requestBody);
+    });
 
-        when('I send a request to create a student', async () => {
-            response = await request(app)
-                .post('/students')
-                .send(requestBody);
-        });
+    then("the student should be created successfully", () => {
+      expect(response.status).toBe(201);
+      expect(response.body.data.name).toBe(requestBody.name);
+    });
+  });
 
-        then('the student should be created successfully', () => {
-            expect(response.status).toBe(201);
-            expect(response.body.data.name).toBe(requestBody.name);
-        });
+  test("Missing Student Email", ({ given, when, then, and }) => {
+    let requestBody: any = {};
+    let response: any = {};
 
-    })
+    given(/^I want to create a student named "(.*)" and no email$/, (name) => {
+      requestBody = {
+        name,
+      };
+    });
 
-    test('Missing Student Email', ({ given, when, then, and }) => {
-        let requestBody: any = {};
-        let response: any = {};
+    when("I send a request to create a student", async () => {
+      response = await request(app).post("/students").send(requestBody);
+    });
 
-        given(/^I want to create a student named "(.*)" and no email$/, (name) => {
-            requestBody = {
-                name
-            }
-        })
+    then("the student should not be created", () => {
+      expect(response.status).toBe(400);
+      expect(response.body.success).toBeFalsy();
+    });
 
-        when('I send a request to create a student', async () => {
-            response = await request(app)
-                .post('/students')
-                .send(requestBody);
-        });
-
-        then('the student should not be created', () => {
-            expect(response.status).toBe(400);
-            expect(response.body.success).toBeFalsy()
-        });
-
-        and('I should receive an error message', () => {
-            expect(response.body.error).toBe('ValidationError');
-        });
-    })
-})
+    and("I should receive an error message", () => {
+      expect(response.body.error).toBe("ValidationError");
+    });
+  });
+});
