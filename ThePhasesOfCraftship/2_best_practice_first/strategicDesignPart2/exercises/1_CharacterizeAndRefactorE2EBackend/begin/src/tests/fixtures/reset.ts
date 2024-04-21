c67d93f1f8ff1c7f108f66bc95286b06cc28cf77
Lucas Machado <lucasmachado.rj@gmail.com>
2024-04-21 00:03:13 -0300

    prepare start project

---
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart2/exercises/1_CharacterizeAndRefactorE2EBackend/begin/src/tests/fixtures/reset.ts
@@ -0,0 +1,25 @@
+import { prisma } from "../../database";
+
+async function resetDatabase() {
+  const deleteAllClassEnrollments = prisma.classEnrollment.deleteMany();
+  const deleteAllStudentAssignments = prisma.studentAssignment.deleteMany();
+  const deleteAllStudents = prisma.student.deleteMany();
+  const deleteAllClasses = prisma.class.deleteMany();
+  const deleteAllAssignments = prisma.assignment.deleteMany();
+
+  try {
+    await prisma.$transaction([
+      deleteAllClassEnrollments,
+      deleteAllStudentAssignments,
+      deleteAllStudents,
+      deleteAllClasses,
+      deleteAllAssignments,
+    ]);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+export { resetDatabase };
