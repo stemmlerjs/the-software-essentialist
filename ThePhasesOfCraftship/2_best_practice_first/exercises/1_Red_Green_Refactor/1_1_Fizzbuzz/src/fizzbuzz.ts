15145ddad96c5fed7e383601930e2c8e9ab0b5cc
Mason Embry <mason.embry@asurion.com>
2024-04-21 08:32:11 -0500

    Red

--- ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,3 +1,21 @@
+// Commit on every step
+
+// each return value is a string
+// 3 returns "Fizz"
+// 5 returns "Buzz
+// 15 returns "FizzBuzz"
+// 9 returns "Fizz"
+// 43 returns "43"
+// 42 returns "Fizz"
+// 45 returns "FizzBuzz"
+// 102 (you decide, throw an Error or handle some other way)
+// -12 (you decide, throw an Error or handle some other way)
+// any non-number (you decide, throw an Error or handle some other way)
+
+// Once I have made the aforementioned tests pass, I have refactored my test specifications to use it.each() to perform parameterization
+
+// There is no duplication in my test code or my production code
+
 export function fizzbuzz(num: number) {
   return "str";
 }
--- ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -4,4 +4,8 @@ describe("fizzbuzz", () => {
   it("returns a string", () => {
     expect(typeof fizzbuzz(5)).toBe("string");
   });
+
+  it("returns 'Fizz' when the input is 3", () => {
+    expect(fizzbuzz(3)).toBe("Fizz");
+  });
 });
