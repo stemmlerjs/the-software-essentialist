0d9a99d770a953e2df2bf054fafc507eb3b8c83b
Mason Embry <mason.embry@asurion.com>
2024-04-21 08:33:19 -0500

    Red

--- ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -8,4 +8,8 @@ describe("fizzbuzz", () => {
   it("returns 'Fizz' when the input is 3", () => {
     expect(fizzbuzz(3)).toBe("Fizz");
   });
+
+  it("returns 'Buzz' when the input is 5", () => {
+    expect(fizzbuzz(5)).toBe("Buzz");
+  });
 });
