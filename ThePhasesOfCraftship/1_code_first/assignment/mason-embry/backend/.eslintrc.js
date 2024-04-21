a2b881e6a657470e232378faa0e17ff1aa1cff87
Mason Embry <mason.embry@asurion.com>
2024-04-21 08:17:39 -0500

    Cleanup

--- ThePhasesOfCraftship/1_code_first/assignment/mason-embry/backend/.eslintrc.js
+++ ThePhasesOfCraftship/1_code_first/assignment/mason-embry/backend/.eslintrc.js
@@ -3,7 +3,11 @@ module.exports = {
     node: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
