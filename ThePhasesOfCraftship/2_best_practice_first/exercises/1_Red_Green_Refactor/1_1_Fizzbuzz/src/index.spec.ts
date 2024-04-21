54ba96e8dffeaec536cb1ce71d40978af018f063
Mason Embry <mason.embry@asurion.com>
2024-04-21 08:34:33 -0500

    Single quotes

--- ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,15 +1,15 @@
-import { fizzbuzz } from "./fizzbuzz";
+import { fizzbuzz } from './fizzbuzz';
 
-describe("fizzbuzz", () => {
-  it("returns a string", () => {
-    expect(typeof fizzbuzz(5)).toBe("string");
+describe('fizzbuzz', () => {
+  it('returns a string', () => {
+    expect(typeof fizzbuzz(5)).toBe('string');
   });
 
   it("returns 'Fizz' when the input is 3", () => {
-    expect(fizzbuzz(3)).toBe("Fizz");
+    expect(fizzbuzz(3)).toBe('Fizz');
   });
 
   it("returns 'Buzz' when the input is 5", () => {
-    expect(fizzbuzz(5)).toBe("Buzz");
+    expect(fizzbuzz(5)).toBe('Buzz');
   });
 });
