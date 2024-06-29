281d26c9aa400893950473ca0f9b498d363bc41b
Lucas Machado <lucasmachado.rj@gmail.com>
2024-06-29 18:23:34 +0000

    remove node_modules

--- ThePhasesOfCraftship/2_best_practice_first/deployment/assignment/end/node_modules/@dddforum/frontend/src/components/content.tsx
+++
@@ -1,6 +0,0 @@
-
-export const Content = ({ children }: any) => (
-  <div className='content-container'>
-    {children}
-  </div>
-)
--- ThePhasesOfCraftship/2_best_practice_first/deployment/assignment/end/node_modules/@dddforum/frontend/tests/support/pages/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './pages';
-export * from './app';
-export * from './pageObject';
