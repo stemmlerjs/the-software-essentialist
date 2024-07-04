c3b3a730027e3c1cc22f95897a172c2ff7a00dbd
Lucas Machado <lucasmachado.rj@gmail.com>
2024-07-04 16:54:07 -0300

    End to end types

--- ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart3/assignment/end/packages/frontend/tests/features/registration.e2e.ts
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart3/assignment/end/packages/frontend/tests/features/registration.e2e.ts
@@ -6,18 +6,22 @@ import * as path from 'path';
 const feature = loadFeature(path.join(sharedTestRoot, 'features/registration.feature'), { tagFilter: '@frontend' });
 
 defineFeature(feature, (test) => {
-  test('Successful registration', ({ given, when, then }) => {
+  test('Successful registration with marketing emails accepted', ({ given, when, then, and }) => {
     
     given('I am a new user', async () => {
       // to be implemented
     });
 
-    when('I register with valid account details', async () => {
+    when('I register with valid account details accepting marketing emails', async () => {
       // to be implemented
     });
 
     then('I should be granted access to my account', async () => {
       // to be implemented
     });
+
+    and('I should expect to receive marketing emails', async () => {
+      // to be implemented
+    })
   });
 });
