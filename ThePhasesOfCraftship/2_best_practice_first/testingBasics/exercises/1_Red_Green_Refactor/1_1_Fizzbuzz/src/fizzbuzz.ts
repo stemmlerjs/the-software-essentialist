fd144bb162bbed41bf502bf1051477d803087df0
enekesabel <enekes.abel@gmail.com>
2024-08-28 14:27:41 +0200

    [green] 102 throws error

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,5 +1,9 @@
 export const fizzBuzz = (value: number ) => {
 
+    if(value === 102){
+        throw new Error('The input must be a number between 1 and 100.')
+    }
+
     if(value % 3 === 0 && value % 5 === 0){
         return 'FizzBuzz';
     }
