64202d6c27b5d66c8121897ba36a563819c14f83
enekesabel <enekes.abel@gmail.com>
2024-08-28 21:24:00 +0200

    [red] returns false for single letter

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,7 +1,12 @@
 import { isPalindrome } from ".";
 
 describe('palindrome checker', () => {
+    
     it('should return true for an empty string', () => {
         expect(isPalindrome("")).toBe(true);
-      });
+    });
+    
+    it('should return true for a single letter', () => {
+        expect(isPalindrome("a")).toBe(true);
+    });
 })
