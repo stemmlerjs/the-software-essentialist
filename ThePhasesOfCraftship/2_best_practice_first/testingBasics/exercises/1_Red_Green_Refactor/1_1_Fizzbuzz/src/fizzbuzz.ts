9d40e73bd88d8b0612e15c7568b2a0fb1763c74d
MateuszMyrcik <matimyrcik@gmail.com>
2024-08-31 16:05:43 +0200

    [green]: fuzzbuzz fn - given 15 return fizzbuzz

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,4 +1,5 @@
 export const fizzbuzz = (number: number) => {
+  if (number % 3 === 0 && number % 5 === 0) return "fizzbuzz";
   if (number % 3 === 0) return "fizz";
   if (number % 5 === 0) return "buzz";
   return "";
