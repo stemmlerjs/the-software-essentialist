a16249c97a5199615670a2c90fdaca54205eac4f
MateuszMyrcik <matimyrcik@gmail.com>
2024-08-31 15:49:56 +0200

    [green]: fuzzbuzz fn - returns string for given number

---
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -0,0 +1,3 @@
+export const fizzbuzz = (number: number) => {
+  return "";
+};
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,3 +1,5 @@
+import { fizzbuzz } from "./fizzbuzz";
+
 describe("fizzbuzz", () => {
   it("should return string", () => {
     expect(typeof fizzbuzz(5)).toBe("string");
