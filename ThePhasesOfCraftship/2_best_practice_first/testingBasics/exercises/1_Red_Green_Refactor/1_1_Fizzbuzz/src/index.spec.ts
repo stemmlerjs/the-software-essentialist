ed52cb204d51abea28c6d87eb9b573930568a120
MateuszMyrcik <matimyrcik@gmail.com>
2024-08-31 15:52:08 +0200

    [red]: given 3 should returned fizz

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -4,4 +4,8 @@ describe("fizzbuzz", () => {
   it("should return string", () => {
     expect(typeof fizzbuzz(5)).toBe("string");
   });
+
+  it("should return fizz for 3", () => {
+    expect(fizzbuzz(3)).toBe("fizz");
+  });
 });
