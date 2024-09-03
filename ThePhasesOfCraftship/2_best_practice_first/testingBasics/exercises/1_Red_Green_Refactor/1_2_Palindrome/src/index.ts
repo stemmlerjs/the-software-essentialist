d901763302ab5921b6fdab550d04a3605ca7cc5c
enekesabel <enekes.abel@gmail.com>
2024-09-03 14:57:28 +0200

    [refactor] split up string cleaning to communicate intent better

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,6 +1,6 @@
 export const isPalindrome = (str: string): boolean =>{
-     // Remove spaces and convert to lowercase
-    const cleanedStr = str.replace(/\s+/g, '').toLowerCase();
+    const strWithoutSpaces = str.replace(/\s+/g, '');
+    const cleanedStr = strWithoutSpaces.toLowerCase();
     
     return cleanedStr === cleanedStr.split('').reverse().join('');
 }
