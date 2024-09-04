85f3a9176d2d48bca33135529d59d821b3787d83
enekesabel <enekes.abel@gmail.com>
2024-09-04 10:30:20 +0200

    docs: gather responsibilities and collaborators

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,3 +1,26 @@
+/*
+
+RESPONSIBILITIES
+
+- take a secuence of integers
+- calculate minimum value
+- calculate maximum value
+- calculate number of elements
+- calculate average value
+- return statistics in result
+
+COLLABORATORS
+
+StatsCalculator
+- take integer sequence
+- calculate statistics
+- construct and return Result
+
+Result
+- store statistics
+
+*/
+
 
 describe('stats calculator', () => {
 
