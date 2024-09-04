22dc43373c18fd9caaa3f8a1b32d891870715bf4
enekesabel <enekes.abel@gmail.com>
2024-09-04 11:27:17 +0200

    docs: remove example test and comments used for planning

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,51 +1,7 @@
-/*
-
-RESPONSIBILITIES
-
-- take a secuence of integers
-- calculate minimum value
-- calculate maximum value
-- calculate number of elements
-- calculate average value
-- return statistics in result
-
-COLLABORATORS
-
-StatsCalculator
-- validate sequence (integers only)
-    - throw error otherwise
-- calculate statistics
-- construct and return Result
-
-Result
-- store statistics
-
-*/
-
 import { calculateStats } from ".";
 
 
 describe('stats calculator', () => {
-
-    it.skip('calculates stats', ()=>{
-        // arrange
-        const sequence = [2, 4, 21, -8, 53, 40];
-
-        // act
-
-        const result = {
-            min: -8,
-            max: 53,
-            count: 6,
-            average: 18.666666666667
-        }
-
-        // assert
-        expect(result.min).toBe(-8);
-        expect(result.max).toBe(53);
-        expect(result.count).toBe(6);
-        expect(result.average).toBe(18.666666666667);
-    })
 
     describe('accepts a sequence of integers', () => {
 
