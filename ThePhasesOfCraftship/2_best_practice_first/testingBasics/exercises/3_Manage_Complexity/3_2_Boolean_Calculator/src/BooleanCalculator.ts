5b4a9db65f380b1f2b241054c60b530507894d83
enekesabel <enekes.abel@gmail.com>
2024-09-11 13:08:07 +0200

    feat: implement BooleanCalculator - fake impl

---
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/BooleanCalculator.ts
@@ -0,0 +1,7 @@
+export class BooleanCalculator {
+
+    public static Evaluate(booleanStr: string): boolean {
+        return true;
+    }
+
+}
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -1,4 +1,15 @@
+import { BooleanCalculator } from "./BooleanCalculator";
 
 describe('boolean calculator', () => {
 
+    it('Should evaluate "TRUE" as true', () => {
+        // arrange
+        const booleanStr = 'TRUE';
+
+        // act
+        const result = BooleanCalculator.Evaluate(booleanStr);
+
+        // assert
+        expect(result).toBe(true);
+    })
 })
