0736c6c392ce784e145f12fe80a5d6fdfa4e14d3
enekesabel <enekes.abel@gmail.com>
2024-09-11 13:33:08 +0200

    feat: evaluate NOT - TPP 6->7

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/BooleanCalculator.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/BooleanCalculator.ts
@@ -1,10 +1,14 @@
+const resolutions = {
+    'TRUE': true,
+    'FALSE': false,
+    'NOT TRUE': false,
+    'NOT FALSE': true
+} as const;
+
 export class BooleanCalculator {
 
     public static Evaluate(booleanStr: string): boolean {
-        if(booleanStr === 'TRUE') {
-            return true;
-        }
-        return false;
+        return resolutions[booleanStr as unknown as keyof typeof resolutions];
     }
 
 }
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -2,10 +2,26 @@ import { BooleanCalculator } from "./BooleanCalculator";
 
 describe('boolean calculator', () => {
 
-    describe('Evaluate simple values', () => {
+    describe('Evaluateing simple values', () => {
         it.each([
             { input: 'TRUE', expected: true },
             { input: 'FALSE', expected: false },
+        ])('Should evaluate "$input" as $expected', ({ input, expected }) => {
+            // arrange
+            const booleanStr = input;
+    
+            // act
+            const result = BooleanCalculator.Evaluate(booleanStr);
+    
+            // assert
+            expect(result).toBe(expected);
+        })
+    })
+
+    describe('Evaluating NOT', () => {
+        it.each([
+            { input: 'NOT TRUE', expected: false },
+            { input: 'NOT FALSE', expected: true },
         ])('Should evaluate "$input" as $expected', ({ input, expected }) => {
             // arrange
             const booleanStr = input;
