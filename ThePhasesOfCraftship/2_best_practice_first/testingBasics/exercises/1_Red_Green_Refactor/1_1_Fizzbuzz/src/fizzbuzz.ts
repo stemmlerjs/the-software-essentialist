f1c3269c3844bf022b1069ba2bcbfd7af204c41b
MateuszMyrcik <matimyrcik@gmail.com>
2024-09-11 21:41:45 +0200

    [refactor]: removed redundant console log

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,5 +1,4 @@
 export const fizzbuzz = (number: number) => {
-  // console.log(new Error("Provide number in range 1 to 100"));
   if (number < 0 || number > 100)
     throw new Error("Provide number in range 1 to 100");
   if (number % 3 === 0 && number % 5 === 0) return "fizzbuzz";
