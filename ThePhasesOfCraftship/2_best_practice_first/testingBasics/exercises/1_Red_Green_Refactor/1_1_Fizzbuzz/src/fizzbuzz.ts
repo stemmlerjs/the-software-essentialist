1f9ad272960446dd43b95293e7d25567e3b6854c
MateuszMyrcik <matimyrcik@gmail.com>
2024-09-14 15:11:46 +0200

    [green]: fuzzbuzz fn - given argument is not type of number

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,4 +1,7 @@
 export const fizzbuzz = (number: number) => {
+  if (typeof number !== "number")
+    throw new Error("Provide argument is not type of number");
+  
   if (number < 0 || number > 100)
     throw new Error("Provide number in range 1 to 100");
 
