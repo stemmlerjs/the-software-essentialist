b069eff6d914920691c4ef13bda53e7f78d48921
MateuszMyrcik <matimyrcik@gmail.com>
2024-10-19 16:42:03 +0200

    [red] should validate capitalise Mom as palindrome

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -4,4 +4,8 @@ describe("palindrome checker", () => {
   test("should validate 'mom' as palindrome", () => {
     expect(isPalindrome("mom")).toBeTruthy();
   });
+
+  test("should validate 'Mom' as palindrome", () => {
+    expect(isPalindrome("Mom")).toBeTruthy();
+  });
 });
