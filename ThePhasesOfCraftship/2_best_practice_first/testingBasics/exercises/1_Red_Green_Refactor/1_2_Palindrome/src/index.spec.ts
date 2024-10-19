8f76f4aa0b168bb9556fc96f91b6ed00c33b1819
MateuszMyrcik <matimyrcik@gmail.com>
2024-10-19 16:50:17 +0200

    [refactor]: improving naming from validate to confirm

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,12 +1,12 @@
 import { isPalindrome } from "./index";
 
 describe("palindrome checker", () => {
-  test("should validate 'mom' as palindrome", () => {
+  test("should confirm that 'mom' is palindrome", () => {
     expect(isPalindrome("mom")).toBeTruthy();
   });
 
-  test.each(["Mom", "MOM"])(
-    "should validate uppercase single word as palindrome",
+  test.each(["Mom", "MoM"])(
+    "should confirm %s is palindrome when is single uppercase word",
     (val) => {
       expect(isPalindrome(val)).toBeTruthy();
     }
