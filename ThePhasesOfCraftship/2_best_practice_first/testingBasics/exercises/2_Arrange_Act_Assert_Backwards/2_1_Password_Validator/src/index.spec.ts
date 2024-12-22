1cfa5a09e576f60fd9939997267147ae80b9252e
zderekh <zahariya.derekh@gmail.com>
2024-12-22 13:58:52 +0100

    feat: implemented length validation

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -1,6 +1,6 @@
 import validatePassword from "./index";
 
-describe('password validator', () => {
+describe('Password validator', () => {
 
 
 // Between 5 and 15 characters long
@@ -8,35 +8,24 @@ describe('password validator', () => {
 // Contains at least one upper case letter
 // Return an object containing a boolean result and an errors key that — when provided with an invalid password — contains an error message or type for all errors in occurrence. There can be multiple errors at a single time.
 
-const output= {
-  result: false,
-  errors:[
-    {
-      type: "length",
-      message: "Password must be between 5 and 15 characters long"
-    },
-    {
-      type: "digit",
-      message: "Password must contain at least one digit"
-    },
-    {
-      type: "upperCase",
-      message: "Password must contain at least one upper case letter"
-    },        
-  ]
-}
-
 
   test('provide a feedback if the password is valid or not, and a list of errors if not', () => {
-    //Setup phase
     const password= "1234567890"
 
-    //Execution phase
     const output = validatePassword(password);
 
-    //Validate phase
     expect(output).toHaveProperty("result");
     expect(output).toHaveProperty("errors");
+
+  })
+
+  test('fails verification if password is not between 5 and 15 characters long', ()=>{
+    const password= "1234"
+    const output = validatePassword(password);
+
+    expect(output.result).toBe(false);
+    expect(output.errors).toHaveLength(1);
+    expect(output.errors[0].type).toBe("length");
 
   })
 })
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
@@ -2,6 +2,11 @@ export default function validatePassword(password: string) {
     let isValid=true;
     let errors: {type: string, message: string}[] = [];
 
+    if(password.length < 5 || password.length > 15){
+        isValid=false;
+        errors.push({type: "length", message: "Password must be between 5 and 15 characters long"});
+    }   
+
     return {
         result: isValid,
         errors: errors
