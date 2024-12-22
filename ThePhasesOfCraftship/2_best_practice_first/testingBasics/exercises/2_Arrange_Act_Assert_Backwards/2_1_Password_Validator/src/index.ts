f29ae703ee806381706781f62bf896b8cd428ed4
zderekh <zahariya.derekh@gmail.com>
2024-12-22 14:02:00 +0100

    refactor: added typings safity

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
@@ -1,10 +1,26 @@
-export default function validatePassword(password: string) {
+enum PasswordValidationErrorType {
+    LENGTH = "length",
+    DIGIT = "digit",
+    UPPER_CASE = "upperCase"
+}
+
+type PasswordValidationResult = {
+    result: boolean;
+    errors: PasswordValidationError[];
+}
+
+type PasswordValidationError = {
+    type: PasswordValidationErrorType;
+    message: string;
+}
+
+export default function validatePassword(password: string): PasswordValidationResult {
     let isValid=true;
-    let errors: {type: string, message: string}[] = [];
+    let errors: PasswordValidationError[] = [];
 
     if(password.length < 5 || password.length > 15){
         isValid=false;
-        errors.push({type: "length", message: "Password must be between 5 and 15 characters long"});
+        errors.push({type: PasswordValidationErrorType.LENGTH, message: "Password must be between 5 and 15 characters long"});
     }   
 
     return {
