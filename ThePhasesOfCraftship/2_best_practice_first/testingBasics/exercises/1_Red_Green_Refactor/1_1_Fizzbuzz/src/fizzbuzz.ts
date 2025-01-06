3875898e6034e26fc04913668cbc20d5751d1a62
Jose Garrera <josegarrera@gmail.com>
2025-01-06 17:33:10 -0300

    [GREEN]: should throw error message "number should be lower or equal to 100" if it get 102

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,5 +1,6 @@
 export const fizzbuzz = (n: number): string => {
     if (n <= 0) throw new Error("number should be greater than zero")
+    if (n > 100) throw new Error("number should be lower or equal to 100")
     if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz"
     if (n % 3 === 0) return "Fizz"
     if (n % 5 === 0) return "Buzz"
