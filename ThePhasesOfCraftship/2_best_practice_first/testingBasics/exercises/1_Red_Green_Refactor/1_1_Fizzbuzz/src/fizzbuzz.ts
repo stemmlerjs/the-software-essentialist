7286efca1f42956cc1e15049a19e97083497c689
Jose Garrera <josegarrera@gmail.com>
2025-01-06 17:11:08 -0300

    [GREEN]: should return FizzBuzz if it get a 15

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,3 +1,5 @@
 export const fizzbuzz = (n: number) => {
-    return n === 3 ? 'Fizz' : 'Buzz'
+    if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz"
+    if (n % 3 === 0) return "Fizz"
+    if (n % 5 === 0) return "Buzz"
 }
