8690df81a60ee5d969aa389a32fb13a19ea88fdf
Jose Garrera <josegarrera@gmail.com>
2025-01-06 16:52:44 -0300

    [RED]: fizzbuzz of 3

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,6 +1,11 @@
 
 describe("fizzbuzz", () => {
+
     it('should compile',  () => {
         expect(true).toBeTruthy();
+    })
+
+    it('should returns "Fizz" if it gets 3', () => {
+        expect(fizzbuzz(3)).toBe("Fizz")
     })
 });
