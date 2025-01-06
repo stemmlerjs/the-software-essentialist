83c4e77b2ba9f85c5f314b3485823e8952d38497
Jose Garrera <josegarrera@gmail.com>
2025-01-06 17:14:08 -0300

    [REFACTOR]: add return type

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,4 +1,4 @@
-export const fizzbuzz = (n: number) => {
+export const fizzbuzz = (n: number): string => {
     if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz"
     if (n % 3 === 0) return "Fizz"
     if (n % 5 === 0) return "Buzz"
