839106b2c50de4176d6e4629b55c7766378ce37e
Jose Garrera <josegarrera@gmail.com>
2025-01-06 17:13:26 -0300

    [GREEN]: should return 1 if it get 1

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -2,4 +2,5 @@ export const fizzbuzz = (n: number) => {
     if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz"
     if (n % 3 === 0) return "Fizz"
     if (n % 5 === 0) return "Buzz"
+    return n.toString()
 }
