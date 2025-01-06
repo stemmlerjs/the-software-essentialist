1105867b4466a711c8cccfbe0851fe95b5de519c
Jose Garrera <josegarrera@gmail.com>
2025-01-06 17:16:39 -0300

    [GREEN]: should throw error if it get 0

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,4 +1,5 @@
 export const fizzbuzz = (n: number): string => {
+    if (n === 0) throw new Error()
     if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz"
     if (n % 3 === 0) return "Fizz"
     if (n % 5 === 0) return "Buzz"
