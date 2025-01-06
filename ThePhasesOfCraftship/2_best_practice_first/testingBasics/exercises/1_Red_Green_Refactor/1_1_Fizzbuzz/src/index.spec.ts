d77382422c6b7222ab329e5d09ab0542b44291fa
Jose Garrera <josegarrera@gmail.com>
2025-01-06 18:03:42 -0300

    [REFACTOR]: use .each

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -2,52 +2,31 @@ import { fizzbuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
     const greaterThanZeroErrorMessage = "number should be greater than zero";
+    const lowerOrEqualTo100ErrorMessage = "number should be lower or equal to 100";
 
-    it('should compile',  () => {
-        expect(true).toBeTruthy();
+    const happyCases = [
+        ["Fizz", 3],
+        ["Buzz", 5],
+        ["FizzBuzz", 15],
+        ["Fizz", 9],
+        ["1", 1],
+        ["Fizz", 42],
+        ["FizzBuzz", 45],
+        ["43", 43],
+
+    ]
+
+    test.each(happyCases)("should return %s if it get %s", (expected, n) => {
+        expect(fizzbuzz(Number(n))).toBe(expected)
     })
 
-    it('should return "Fizz" if it get 3', () => {
-        expect(fizzbuzz(3)).toBe("Fizz")
-    })
+    const unHappyCases = [
+        [greaterThanZeroErrorMessage, 0],
+        [greaterThanZeroErrorMessage, -10],
+        [lowerOrEqualTo100ErrorMessage, 102]
+    ]
 
-    it('should return "Buzz" if it get a 5', () => {
-        expect(fizzbuzz(5)).toBe("Buzz")
-    })
-
-    it('should return "FizzBuzz" if it get a 15', () => {
-        expect(fizzbuzz(15)).toBe("FizzBuzz")
-    })
-
-    it('should return "Fizz" if it get 9', () => {
-        expect(fizzbuzz(9)).toBe("Fizz")
-    })
-
-    it('should return "1" if it get 1', () => {
-        expect(fizzbuzz(1)).toBe("1")
-    })
-
-    it('should return "Fizz" if it get 42', () => {
-        expect(fizzbuzz(42)).toBe("Fizz")
-    })
-
-    it('should return "FizzBuzz" if it get a 45', () => {
-        expect(fizzbuzz(45)).toBe("FizzBuzz")
-    })
-
-    it('should return "43" if it get 43', () => {
-        expect(fizzbuzz(43)).toBe("43")
-    })
-
-    it('should throw error message "number should be greater than zero" if it get 0', () => {
-        expect(() => fizzbuzz(0)).toThrow(greaterThanZeroErrorMessage)
-    })
-
-    it('should throw error message "number should be greater than zero" if it get -10', () => {
-        expect(() => fizzbuzz(-10)).toThrow(greaterThanZeroErrorMessage)
-    })
-
-    it('should throw error message "number should be lower or equal to 100" if it get 102', () => {
-        expect(() => fizzbuzz(102)).toThrow("number should be lower or equal to 100")
+    test.each(unHappyCases)("should throw error message %s if it get %s", (expected, n) => {
+        expect(()=> fizzbuzz(Number(n))).toThrow(String(expected))
     })
 });
