57ef45ad35c2972a29d472536b8e105d60000fd8
Jose Garrera <josegarrera@gmail.com>
2025-01-14 18:23:11 -0300

    [GREEN]: should know that for the numbers 1, 2 and 3, the minimum is 1

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,5 +1,10 @@
+import {StatsCalculator} from "./index";
 
 describe('stats calculator', () => {
-
-
+    it('should know that for the numbers 1, 2 and 3, the minimum is 1', () => {
+        const result = {
+            min: 1,
+        }
+        expect(StatsCalculator.run()).toEqual(result);
+    })
 })
---
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.ts
@@ -0,0 +1,7 @@
+export class StatsCalculator {
+    static run() {
+        return {
+            min: 1
+        }
+    }
+}
