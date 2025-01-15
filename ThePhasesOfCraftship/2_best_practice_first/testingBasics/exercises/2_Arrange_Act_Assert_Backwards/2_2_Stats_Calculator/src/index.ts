74c6b691c7d6e42fc4a4a87bbf736290c75f5117
Jose Garrera <josegarrera@gmail.com>
2025-01-15 14:02:54 -0300

    [REFACTOR]: add return type

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.ts
@@ -1,5 +1,12 @@
+export type Statistics = {
+    min: number,
+    max: number,
+    numberOfElements: number,
+    average: number
+}
+
 export class StatsCalculator {
-    static run() {
+    static run(): StatsCalculator {
         return {
             min: 1,
             max: 3
