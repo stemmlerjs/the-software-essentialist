29d71ead845d17c20d24716e1faafb47f117ee5a
Jose Garrera <josegarrera@gmail.com>
2025-01-15 13:47:30 -0300

    [REFACTOR]: improved test

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -2,9 +2,7 @@ import {StatsCalculator} from "./index";
 
 describe('stats calculator', () => {
     it('should know that for the numbers 1, 2 and 3, the minimum is 1', () => {
-        const result = {
-            min: 1,
-        }
-        expect(StatsCalculator.run()).toEqual(result);
+        expect(StatsCalculator.run()).toHaveProperty('min')
+        expect(StatsCalculator.run().min).toBe(1)
     })
 })
