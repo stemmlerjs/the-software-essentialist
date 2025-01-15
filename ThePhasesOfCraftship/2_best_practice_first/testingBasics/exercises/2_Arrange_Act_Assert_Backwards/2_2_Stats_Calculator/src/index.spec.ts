25878cbe7fa8bf6224fdf8ce91a7cb009d538dd3
Jose Garrera <josegarrera@gmail.com>
2025-01-15 15:08:14 -0300

    [REFACTOR]: it.each for happy cases

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,52 +1,36 @@
 import {Statistics, StatsCalculator} from "./index";
 
-describe('stats calculator', () => {
-    it("should know that for the numbers 1, 2 and 3, the minimum is 1, the maximum is 3, the number of elements is 3 and the average is 2",  () => {
-        const numbers: number[] = [1, 2, 3];
+type HappyCase = [number[], Statistics];
 
-        const expected: Statistics = {
+describe('stats calculator', () => {
+    const happyCases: HappyCase[] = [
+        [[1, 2, 3], {
             min: 1,
             max: 3,
             numberOfElements: 3,
             average: 2
-        }
-        expect(StatsCalculator.run(numbers)).toEqual(expected);
-    })
-
-    it('should know that for the numbers 2, 3, 1 and 6 the minimum is 1, the maximum is 6, the number of elements is 4 and the average is 3', () => {
-        const numbers: number[] = [2, 3, 1, 6];
-
-        const expected: Statistics = {
+        }],
+        [[2, 3, 1, 6], {
             min: 1,
             max: 6,
             numberOfElements: 4,
             average: 3
-        }
-        expect(StatsCalculator.run(numbers)).toEqual(expected);
-    })
-
-    it('should know that for the numbers 2, 4, 21, -8, 53 and 40 the minimum is -8, the maximum is 53, the number of elements is 6 and the average is 18.666666666666668', () => {
-        const numbers: number[] = [2, 4, 21, -8, 53, 40];
-
-        const expected: Statistics = {
+        }],
+        [[2, 4, 21, -8, 53, 40], {
             min: -8,
             max: 53,
             numberOfElements: 6,
             average: 18.666666666666668
-        }
-        expect(StatsCalculator.run(numbers)).toEqual(expected);
-    })
-
-    it('should know that for the numbers 0, 0 and 0 the minimum is 0, the maximum is 0, the number of elements is 3 and the average is 0', () => {
-        const numbers: number[] = [0, 0, 0];
-
-        const expected: Statistics = {
+        }],
+        [[0, 0, 0], {
             min: 0,
             max: 0,
             numberOfElements: 3,
             average: 0
-        }
-        expect(StatsCalculator.run(numbers)).toEqual(expected);
+        }]
+    ];
+    it.each(happyCases)("should know that for the numbers %s the result should be %s", (numbers, expected) => {
+        expect(StatsCalculator.run(numbers)).toEqual(expected)
     })
 
     it('should throw an error message \"Input contains invalid numbers (e.g., NaN or non-numeric values).\" if the list contains a non number', () => {
