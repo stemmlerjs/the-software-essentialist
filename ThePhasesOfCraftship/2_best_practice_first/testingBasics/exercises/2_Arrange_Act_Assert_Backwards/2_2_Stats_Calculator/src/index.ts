f92be73fbcd74b70f13f6dcf543a368f38727ad2
Jose Garrera <josegarrera@gmail.com>
2025-01-15 14:21:07 -0300

    [REFACTOR]: extract method

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.ts
@@ -7,16 +7,29 @@ export type Statistics = {
 
 export class StatsCalculator {
     static run(numbers: number[]): Statistics {
-        const min = numbers.reduce((min, current) => current < min ? current : min);
-        const max = numbers.reduce((max, current) => current > max ? current : max);
+        const min = this.getMin(numbers);
+        const max = this.getMax(numbers);
         const numberOfElements = numbers.length;
-        const sum = numbers.reduce((total, current) => total + current, 0);
+        const sum = this.getSum(numbers);
         const average = sum / numberOfElements;
+
         return {
             min,
             max,
             numberOfElements,
             average
         }
+    }
+
+    private static getMin(numbers: number[]) {
+        return numbers.reduce((min, current) => current < min ? current : min);
+    }
+
+    private static getMax(numbers: number[]) {
+        return numbers.reduce((max, current) => current > max ? current : max);
+    }
+
+    private static getSum(numbers: number[]) {
+        return numbers.reduce((total, current) => total + current, 0);
     }
 }
