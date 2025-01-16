fb2f3c7737099141feaeab6b869c177200432aa9
Jose Garrera <josegarrera@gmail.com>
2025-01-16 08:48:32 -0300

    [GREEN]: should know that the time "25:00 - 12:23" is not valid

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -5,4 +5,9 @@ describe('military time validator', () => {
         const time = "01:12 - 14:32";
         expect(MilitaryTimeValidator.validate(time)).toBeTruthy();
     })
+
+    it('should know that the time "25:00 - 12:23" is not valid', () => {
+        const time = "25:00 - 12:23";
+        expect(MilitaryTimeValidator.validate(time)).toBeFalsy();
+    })
 })
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,5 +1,19 @@
 export class MilitaryTimeValidator {
-    static validate(time: string): boolean {
-        return true;
+    static validate(times: string): boolean {
+        const [timeFrom, timeTo] = times.split(' - ');
+        return (this.validateTime(timeFrom) && this.validateTime(timeTo));
+    }
+
+    private static validateTime(time: string): boolean {
+        const [hours, minutes] = time.split(':');
+        return (this.validateHours(hours) && this.validateMinutes(minutes));
+    }
+
+    private static validateHours(hours: string): boolean {
+        return (Number(hours) >= 0 && Number(hours) <= 23);
+    }
+
+    private static validateMinutes(minutes: string): boolean {
+        return (Number(minutes) >= 0 && Number(minutes) <= 59);
     }
 }
