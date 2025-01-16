55c2382b88f07d595fa951ebe0b72d82f8c4cf6d
Jose Garrera <josegarrera@gmail.com>
2025-01-16 08:52:01 -0300

    [REFACTOR]: add domain types

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,13 +1,13 @@
-import {MilitaryTimeValidator} from "./index";
+import {MilitaryTimeValidator, Time} from "./index";
 
 describe('military time validator', () => {
     it('should know that the time "01:12 - 14:32" is valid', () => {
-        const time = "01:12 - 14:32";
+        const time: Time = "01:12 - 14:32";
         expect(MilitaryTimeValidator.validate(time)).toBeTruthy();
     })
 
     it('should know that the time "25:00 - 12:23" is not valid', () => {
-        const time = "25:00 - 12:23";
+        const time: Time = "25:00 - 12:23";
         expect(MilitaryTimeValidator.validate(time)).toBeFalsy();
     })
 })
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,19 +1,22 @@
+export type Time = string;
+export type Result = true | false;
+
 export class MilitaryTimeValidator {
-    static validate(times: string): boolean {
+    static validate(times: Time): Result {
         const [timeFrom, timeTo] = times.split(' - ');
         return (this.validateTime(timeFrom) && this.validateTime(timeTo));
     }
 
-    private static validateTime(time: string): boolean {
+    private static validateTime(time: Time): Result {
         const [hours, minutes] = time.split(':');
         return (this.validateHours(hours) && this.validateMinutes(minutes));
     }
 
-    private static validateHours(hours: string): boolean {
+    private static validateHours(hours: Time): Result {
         return (Number(hours) >= 0 && Number(hours) <= 23);
     }
 
-    private static validateMinutes(minutes: string): boolean {
+    private static validateMinutes(minutes: Time): Result {
         return (Number(minutes) >= 0 && Number(minutes) <= 59);
     }
 }
