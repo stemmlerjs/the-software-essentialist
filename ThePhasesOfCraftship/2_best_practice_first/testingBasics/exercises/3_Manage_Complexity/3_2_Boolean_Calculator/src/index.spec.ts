b609d2149895db0adc5a3dbbf95b7ed687619c28
Jose Garrera <josegarrera@gmail.com>
2025-01-17 16:04:42 -0300

    [green]: should know that the result for "TRUE" is true

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -1,4 +1,10 @@
+import {BooleanCalculator} from "./index";
 
 describe('boolean calculator', () => {
-
+    describe('single values', () => {
+        it('should know that the result for "TRUE" is true',  () => {
+            const expression = 'TRUE';
+            expect(BooleanCalculator.run(expression)).toBeTruthy();
+        })
+    })
 })
---
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -0,0 +1,8 @@
+export type Expression = string;
+export type Result = boolean;
+
+export class BooleanCalculator {
+    static run(expression: Expression): Result {
+        return true
+    }
+}
