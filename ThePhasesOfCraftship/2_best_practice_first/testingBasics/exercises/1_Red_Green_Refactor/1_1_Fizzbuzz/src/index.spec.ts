6a2b24689cc3b0e197e5a47d4d0f96a282dacc83
Marc Kalil <marc@marckalil.com>
2025-02-06 15:58:48 +0100

    [RED] each return value is a string

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,8 @@
-
-describe("fizzbuzz", () => {
-
-
+describe("fizzBuzz", () => {
+	describe("each return value is a string", () => {
+		const returnsStringCases = [1, 3, 5, 15];
+		test.each(returnsStringCases)("fizzBuzz(%s) a string", (input: number) => {
+			expect(typeof fizzBuzz(input)).toBe("string");
+		});
+	});
 });
