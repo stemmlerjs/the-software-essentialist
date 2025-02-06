f4c2d599d8ba69c37c322e075749218d6edb361f
Marc Kalil <marc@marckalil.com>
2025-02-06 16:33:56 +0100

    [REFACTOR] 9 returns 'Fizz'

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,17 +1,19 @@
 import { fizzBuzz } from "./fizzbuzz";
 
 describe("fizzBuzz", () => {
-	describe("each return value is a string", () => {
+	describe("Each return value is a string", () => {
 		const returnsStringCases = [1, 3, 5, 15];
 		test.each(returnsStringCases)("fizzBuzz(%s) returns a string", (input: number) => {
 			expect(typeof fizzBuzz(input)).toBe("string");
 		});
 	});
-	test("3 returns 'Fizz'", () => {
-		expect(fizzBuzz(3)).toBe("Fizz");
-	});
-	test("9 returns 'Fizz'", () => {
-		expect(fizzBuzz(9)).toBe("Fizz");
+	describe("When the input is divisible by 3", () => {
+		describe("it returns 'Fizz'", () => {
+			const divisibleBy3Cases = [3, 9, 42];
+			test.each(divisibleBy3Cases)("fizzBuzz(%s) returns 'Fizz'", (input: number) => {
+				expect(fizzBuzz(input)).toBe("Fizz");
+			});
+		});
 	});
 	test("5 returns 'Buzz'", () => {
 		expect(fizzBuzz(5)).toBe("Buzz");
