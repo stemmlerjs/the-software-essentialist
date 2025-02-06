a84462e23c302e3ef88539bfa34fdc2751aaa960
Marc Kalil <marc@marckalil.com>
2025-02-06 16:40:06 +0100

    [GREEN] 15 returns 'FizzBuzz'

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,6 +1,9 @@
 export function fizzBuzz(inputNumber: number): string {
 	const isDivisibleBy3 = inputNumber % 3 === 0;
 	const isDivisibleBy5 = inputNumber % 5 === 0;
+	const isDivisibleBy3And5 = isDivisibleBy3 && isDivisibleBy5;
+	
+	if (isDivisibleBy3And5) return 'FizzBuzz';
 	if (isDivisibleBy3) return 'Fizz';
 	if (isDivisibleBy5) return 'Buzz';
 	return `${inputNumber}`;
