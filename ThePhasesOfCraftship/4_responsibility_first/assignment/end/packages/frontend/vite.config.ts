3094804de3f13be487dede065c88105c469f61b1
Khalil Stemmler <metroidman12@gmail.com>
2025-02-24 23:02:11 -0500

    Got rid of vite config changes.

--- ThePhasesOfCraftship/4_responsibility_first/assignment/end/packages/frontend/vite.config.ts
+++ ThePhasesOfCraftship/4_responsibility_first/assignment/end/packages/frontend/vite.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    preserveSymlinks: true,
-    alias: {
-      '@dddforum/shared': path.resolve(__dirname, '../shared/src')
-    }
-  }
 })
