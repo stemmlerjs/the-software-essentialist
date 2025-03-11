3507c2d6e6374547b31d25229c2dc3ffdbc11118
Khalil Stemmler <metroidman12@gmail.com>
2025-03-11 16:40:11 -0400

    Fixed api -> core, errors build & typecheck experiment.

--- ThePhasesOfCraftship/5_value_first/exercises/1_frontend-fa2str/packages/core/src/utils/numberUtil.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NumberUtil = void 0;
-class NumberUtil {
-    static generateRandomInteger(min, max) {
-        return Math.floor(min + Math.random() * (max - min + 1));
-    }
-}
-exports.NumberUtil = NumberUtil;
